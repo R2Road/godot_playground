2702f3c5a8cba80edc9e265938d4ea2217589857
r2road <studior2road@gmail.com>
2020-09-06 15:00:50 +0900

    update step : keyboard input

--- 2d_ground/gds_step_01_keyboard_input/gds_step_keyboard_input.gd
+++ 2d_ground/gds_step_01_keyboard_input/gds_step_keyboard_input.gd
@@ -1,5 +1,17 @@
 extends Node
 
+<<<<<<< HEAD
+func _ready():
+	set_process_input( true )
+
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		if event.scancode == KEY_A:
+			print( "target key pressed : ", event.as_text() )
+		else:
+			print( "other key pressed : ", event.as_text() )
+=======
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		print( event.as_text() )
+>>>>>>> 0dda8f19f34a98a9579f6d13c236f806468c8f64
