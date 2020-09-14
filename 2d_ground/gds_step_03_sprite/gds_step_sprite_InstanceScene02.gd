71bf10079728e0c6f237ab60521cb41e830c7f9b
r2road <studior2road@gmail.com>
2020-09-15 01:48:05 +0900

    add scene : gds_step_sprite_InstanceScene02

---
+++ 2d_ground/gds_step_03_sprite/gds_step_sprite_InstanceScene02.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _ready():
+	var sprite_resource_actor0001_idle_01 = load( "res://resources/sprite/actor001/actor001_idle_01.png" )
+	
+	var sprite_node = Sprite.new()
+	sprite_node.texture = sprite_resource_actor0001_idle_01
+	sprite_node.position = Vector2( get_viewport().size.x * 0.5, get_viewport().size.y * 0.5 )
+	sprite_node.scale = Vector2( 4, 4 )
+	#sprite_node.scale.x = 4
+	#sprite_node.scale.y = 4
+	
+	add_child( sprite_node )
