8d9c2bd64a7021c847db67ca0759d172a6491f33
r2road <studior2road@gmail.com>
2020-09-21 17:26:06 +0900

    set_position

--- 2d_ground/PlayGround.gd
+++ 2d_ground/PlayGround.gd
@@ -8,3 +8,8 @@ var title_string = \
 func _ready():
 	var summury_node = get_node( "Summury" )
 	summury_node.text = title_string
+	
+	summury_node.set_position(
+		summury_node.get_position()
+		- Vector2( summury_node.rect_size.x * 0.5, summury_node.rect_size.y * 0.5 )
+	)
