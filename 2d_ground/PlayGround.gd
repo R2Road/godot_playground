062a6c90146322aaf09ec602d5f18e28829783c3
r2road <studior2road@gmail.com>
2020-09-22 00:08:22 +0900

    add method : _input

--- 2d_ground/PlayGround.gd
+++ 2d_ground/PlayGround.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+
 var title_string = \
 	"Play Ground" \
 	+ "\n" \
 	+ "[ESC] Exit"
+
 
 func _ready():
 	var summury_node = get_node( "Summury" )
@@ -13,3 +15,12 @@ func _ready():
 		summury_node.get_position()
 		- Vector2( summury_node.rect_size.x * 0.5, summury_node.rect_size.y * 0.5 )
 	)
+	
+	set_process_input( true ) # is not need : default on
+	
+	
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		if event.scancode == KEY_ESCAPE:
+			set_process_input( false )
+			get_tree().quit()
