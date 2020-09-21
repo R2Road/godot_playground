affb2ef3a45a66cde847bd027ca3d6da9dd020cb
r2road <studior2road@gmail.com>
2020-09-22 00:23:16 +0900

    add scene : gds basic - RootScene

---
+++ 2d_ground/gds_basic/gds_basic_RootScene.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+var title_string = \
+	"+ gds basic Root" \
+	+ "\n" \
+	+ "[ESC] Exit"
+
+
+func _ready():
+	var summury_node = get_node( "Summury" )
+	summury_node.text = title_string
+	
+	summury_node.set_position(
+		summury_node.get_position()
+		- Vector2( summury_node.rect_size.x * 0.5, summury_node.rect_size.y * 0.5 )
+	)
+	
+	set_process_input( true ) # is not need : default on
+	
+	
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		if event.scancode == KEY_ESCAPE:
+			set_process_input( false )
+			get_tree().quit()
