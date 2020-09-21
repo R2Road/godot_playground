34ae1a0d886e88bb57c417d5096e3ecc92d26194
r2road <studior2road@gmail.com>
2020-09-22 00:52:41 +0900

    use get_minimum_size instead rect

--- 2d_ground/gds_basic/gds_basic_RootScene.gd
+++ 2d_ground/gds_basic/gds_basic_RootScene.gd
@@ -4,7 +4,13 @@ extends Node2D
 var title_string = \
 	"+ gds basic Root" \
 	+ "\n" \
-	+ "[ESC] Exit"
+	+ "[ESC] Exit" \
+	+ "\n\n" \
+	+ "[1] Empty" \
+	+ "\n\n" \
+	+ "[1] Empty" \
+	+ "\n\n" \
+	+ "[1] Empty"
 
 
 func _ready():
@@ -12,8 +18,8 @@ func _ready():
 	summury_node.text = title_string
 	
 	summury_node.set_position(
-		summury_node.get_position()
-		- Vector2( summury_node.rect_size.x * 0.5, summury_node.rect_size.y * 0.5 )
+		Vector2( get_viewport().size.x * 0.5, get_viewport().size.y * 0.5 )
+		- Vector2( summury_node.get_minimum_size().x * 0.5, summury_node.get_minimum_size().y * 0.5 )
 	)
 	
 	set_process_input( true ) # is not need : default on
