9051d97bcdb79b6c1f1f3ae4fcb078c7dad78384
r2road <studior2road@gmail.com>
2020-09-28 18:52:02 +0900

    cleanup

--- 2d_ground/PlayGround.gd
+++ 2d_ground/PlayGround.gd
@@ -1,27 +1,26 @@
 extends Node2D
 
 
-var title_string = \
-		"+ Play Ground" \
-		+ "\n" \
-		+ "[ESC] Exit" \
-		+ "\n\n" \
-		+ "[1] Basic" \
-		+ "\n" \
-		+ "[2] UI" \
-		+ ""
-
-
 func _ready():
+	set_process_input( true ) # is not need : default on
+	
+	var title_string = \
+			"+ Play Ground" \
+			+ "\n" \
+			+ "[ESC] Exit" \
+			+ "\n\n" \
+			+ "[1] Basic" \
+			+ "\n" \
+			+ "[2] UI" \
+			+ ""
+	
 	var summury_node = get_node( "Summury" )
 	summury_node.text = title_string
 	
 	summury_node.set_position(
-		summury_node.get_position()
+		Vector2( get_viewport().size.x * 0.5, get_viewport().size.y * 0.5 )
 		- Vector2( summury_node.get_minimum_size().x * 0.5, summury_node.get_minimum_size().y * 0.5 )
 	)
-	
-	set_process_input( true ) # is not need : default on
 	
 	
 func _input(event):
