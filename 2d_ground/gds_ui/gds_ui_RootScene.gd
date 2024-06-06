08446853968612f5c9cbd2f32a4a4768628027f9
R2Road <studior2road@gmail.com>
2024-06-06 15:49:58 +0900

    apply

--- 2d_ground/gds_ui/gds_ui_RootScene.gd
+++ 2d_ground/gds_ui/gds_ui_RootScene.gd
@@ -1,35 +1,35 @@
 extends Node2D
 
 
+############################ Variable ############################
+var next_scene_manager = NextSceneManager.new(	[
+	  NextSceneInfo.new( "Return to Root",  Key.KEY_ESCAPE, "res://playground_main.tscn" )
+	, NextSceneInfo.new( "Label",  Key.KEY_1, "res://gds_ui/gds_ui_LabelScene.tscn" )
+] )
+
+
+
+############################ Override ############################
 func _ready():
 	set_process_input( true ) # is not need : default on
-
-	var title_string = \
-			"+ UI" \
-			+ "\n" \
-			+ "[ESC] Return to Root" \
-			+ "\n\n" \
-			+ "[1] Label" \
-			+ ""
 		
 	var summury_node = get_node( "Summury" )
-	summury_node.text = title_string
-	
+	summury_node.text = next_scene_manager.build_summary( "UI" )
 	summury_node.set_position(
 		Vector2( get_viewport().size.x * 0.5, get_viewport().size.y * 0.5 )
 		- Vector2( summury_node.get_minimum_size().x * 0.5, summury_node.get_minimum_size().y * 0.5 )
 	)
-	
-	
+
+
 func _input(event):
 	if !(event is InputEventKey ):
 		return
 	
 	if !event.is_pressed():
 		return
-		
-	match event.keycode:
-		KEY_ESCAPE:
-			get_tree().change_scene_to_file("res://playground_main.tscn")
-		KEY_1:
-			get_tree().change_scene_to_file("res://gds_ui/gds_ui_LabelScene.tscn")
+	
+	var next_scene_path = next_scene_manager.get_next_scene( event.keycode )
+	if( next_scene_path.is_empty() ):
+		return
+	
+	get_tree().change_scene_to_file( next_scene_path )
