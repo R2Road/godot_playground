0e992aa87e915b46b1d471bf8f2302a9f199b33c
R2Road <studior2road@gmail.com>
2024-06-06 16:11:42 +0900

    add scene : gds_sound_root

--- 2d_ground/gds_sound/gds_sound_root.gd
+++ 2d_ground/gds_sound/gds_sound_root.gd
@@ -1,23 +1,20 @@
 extends Node2D
 
 
-
 ############################ Variable ############################
 var next_scene_manager = NextSceneManager.new(	[
-	  NextSceneInfo.new( "Exit",  Key.KEY_ESCAPE, "res://playground_exit.tscn" )
-	, NextSceneInfo.new( "Basic",  Key.KEY_1, "res://gds_basic/gds_basic_root.tscn" )
-	, NextSceneInfo.new( "UI", Key.KEY_2, "res://gds_ui/gds_ui_root.tscn" )
-	, NextSceneInfo.new( "Sound", Key.KEY_3, "res://gds_sound/gds_sound_root.tscn" )
+	  NextSceneInfo.new( "Return to Root",  Key.KEY_ESCAPE, "res://playground_main.tscn" )
 ] )
+#, NextSceneInfo.new( "Label",  Key.KEY_1, "res://gds_sound/gds_sound______.tscn" )
 
 
 
 ############################ Override ############################
 func _ready():
 	set_process_input( true ) # is not need : default on
-	
+		
 	var summury_node = get_node( "Summury" )
-	summury_node.text = next_scene_manager.build_summary( "PlayGround" )	
+	summury_node.text = next_scene_manager.build_summary( "Sound" )
 	summury_node.set_position(
 		Vector2( get_viewport().size.x * 0.5, get_viewport().size.y * 0.5 )
 		- Vector2( summury_node.get_minimum_size().x * 0.5, summury_node.get_minimum_size().y * 0.5 )
