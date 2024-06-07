fbc2709210c2c18d53cf0d84c458b7cbfd223527
R2Road <studior2road@gmail.com>
2024-06-07 15:53:07 +0900

    gdscript lambda has auto capture

--- 2d_ground/playground_action.gd
+++ 2d_ground/playground_action.gd
@@ -5,7 +5,6 @@ class_name PlayGroundAction
 ############################ Variable ############################
 var name : String
 var key : Key
-var scene_path : String
 var action = func dummy():
 	pass
 
@@ -15,7 +14,6 @@ var action = func dummy():
 func _init( _name : String, _key : Key, _scene_path : String )->void:
 	name = _name
 	key = _key
-	scene_path = _scene_path
 	
 	action = func ( node : Node ):
-		node.get_tree().change_scene_to_file( scene_path )
+		node.get_tree().change_scene_to_file( _scene_path )
