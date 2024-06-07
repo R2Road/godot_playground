ffad24bb028c4a4c5f7ba8a358d7dcef50460ba2
R2Road <studior2road@gmail.com>
2024-06-07 17:32:42 +0900

    ...

--- 2d_ground/gds_basic/gds_basic_tree_order_helper.gd
+++ 2d_ground/gds_basic/gds_basic_tree_order_helper.gd
@@ -1,20 +1,25 @@
 extends Node
 
+
+
+############################ Override ############################
 func _init():
 	# Note: a Node doesn't have a "name" yet here.
+	print( name + " init" )
 
-	print(name + " init")
 
 func _enter_tree():
-	print(name + " enter tree")
+	print( name + " enter tree" )
+
 
 func _ready():
-	print(name + " ready")
+	print( name + " ready" )
+
 
 # This ensures we only print *once* in process().
 
 var test = true
 func _process( _delta ):
 	if test:
-		print(name + " process")
+		print( name + " process" )
 		test = false
