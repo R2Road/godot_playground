ad8126cbe70086705e92a7fdec40afcdecd0ddcf
R2Road <studior2road@gmail.com>
2024-06-07 17:27:49 +0900

    rename

--- 2d_ground/gds_basic/gds_basic_root.gd
+++ 2d_ground/gds_basic/gds_basic_root.gd
@@ -7,7 +7,7 @@ var playground_action_manager = PlayGroundActionManager.new( self, "GD Script Ba
 	  PlayGroundAction.new_mover( "Return to Root", 	Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	, PlayGroundAction.new_mover( "Empty",  			Key.KEY_1, 		"res://gds_basic/gds_basic_EmptyScene.tscn" )
 	, PlayGroundAction.new_mover( "OS", 				Key.KEY_2, 		"res://gds_basic/gds_basic_OSScene.tscn" )
-	, PlayGroundAction.new_mover( "Tree Order", 		Key.KEY_3, 		"res://gds_basic/gds_basic_TreeOrderScene.tscn" )
+	, PlayGroundAction.new_mover( "Tree Order", 		Key.KEY_3, 		"res://gds_basic/gds_basic_tree_order.tscn" )
 	, PlayGroundAction.new_mover( "Log", 			Key.KEY_4, 		"res://gds_basic/gds_basic_LogScene.tscn" )
 	, PlayGroundAction.new_mover( "Keyboard Input", 	Key.KEY_5, 		"res://gds_basic/gds_basic_KeyboardInputScene.tscn" )
 	, PlayGroundAction.new_mover( "Get Node", 		Key.KEY_6, 		"res://gds_basic/gds_basic_GetNodeScene.tscn" )
