aed1fe505df2789cd6f508bbb9a1046472ca7dfe
R2Road <studior2road@gmail.com>
2024-06-10 14:50:34 +0900

    ...

--- 2d_ground/gds_basic/gds_basic_root.gd
+++ 2d_ground/gds_basic/gds_basic_root.gd
@@ -11,7 +11,7 @@ func _init():
 	pam.set_name( "GD Script Basic" )
 	pam.add_mover( "Return to Root", 	Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	pam.add_mover( "Empty",  			Key.KEY_1, 		"res://gds_basic/gds_basic_EmptyScene.tscn" )
-	pam.add_mover( "Path",  			Key.KEY_2, 		"res://gds_basic/gds_basic_devpath.tscn" )
+	pam.add_mover( "Dev Path",  		Key.KEY_2, 		"res://gds_basic/gds_basic_devpath.tscn" )
 	pam.add_mover( "Tree",  			Key.KEY_3, 		"res://gds_basic/gds_basic_tree.tscn" )
 	pam.add_mover( "OS", 				Key.KEY_4, 		"res://gds_basic/gds_basic_OSScene.tscn" )
 	pam.add_mover( "Tree Order", 		Key.KEY_5, 		"res://gds_basic/gds_basic_tree_order.tscn" )
