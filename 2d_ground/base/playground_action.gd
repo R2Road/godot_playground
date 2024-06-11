7336f3344d2af97996db00768a98f9d485623909
R2Road <studior2road@gmail.com>
2024-06-11 22:41:24 +0900

    use free instead queue_free : PlaygroundAction은 call_deferred 로 작동하니 문제 없다.

--- 2d_ground/base/playground_action.gd
+++ 2d_ground/base/playground_action.gd
@@ -28,19 +28,32 @@ static func new_mover( _owner : Node, _name : String, _key : Key, _next_scene_pa
 	var ret = PlayGroundAction.new()
 	
 	ret.name = _name
-	ret.key = _key	
+	ret.key = _key
 	ret.action = func ():
-		# 현재 Scene의 모든 개체를 제거한다.
-		for c in _owner.get_tree().root.get_children():
-			c.queue_free()
-		
+		#
 		# SceneTree 의 change scene 을 사용하지 않고 scene 전환 효과를 만든다.
+		#
 		var next_scene = ResourceLoader.load( _next_scene_path ).instantiate()
 		if next_scene is PlaygroundScene:
 			next_scene.pam.set_last_scene( _owner.pam.name, _owner.scene_file_path )
 		else:
 			print_debug( "warning : is not PlayGroundScene" )
-		_owner.get_tree().root.add_child( next_scene )
+		
+		#
+		# 현재 Scene의 모든 개체를 제거한다.
+		#
+		var tree = _owner.get_tree()
+		for c in tree.root.get_children():
+			c.free()
+		
+		#
+		# Scene 설정
+		#
+		tree.root.add_child( next_scene )
+		# current_scene 을 설정하지 않으면
+		# change_scene_to_file 을 이용한 Scene전환 이후
+		# 이전 Scene이 남아 있는 경우가 있다.
+		tree.set_current_scene( next_scene )
 	
 	return ret
 
