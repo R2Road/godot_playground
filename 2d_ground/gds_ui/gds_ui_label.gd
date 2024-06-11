e977e717a19cae340f5509ee835ee78e782b5fc3
R2Road <studior2road@gmail.com>
2024-06-11 16:02:32 +0900

    apply

--- 2d_ground/gds_ui/gds_ui_label.gd
+++ 2d_ground/gds_ui/gds_ui_label.gd
@@ -1,51 +1,26 @@
-extends Node2D
+extends PlaygroundScene
 
 
+
+############################ Override ############################
 func _ready():
-	set_process_input( true ) # is not need : default on
-
-	var title_string = \
-			"+ Empty" \
-			+ "\n" \
-			+ "[ESC] Return to Root" \
-			+ "\n\n" \
-			+ "[A] Change Color : Blue" \
-			+ "\n" \
-			+ "[S] Change Color : Green" \
-			+ "\n" \
-			+ "[D] Append Text" \
-			+ ""
-		
-	var summury_node = get_node( "Summury" )
-	summury_node.text = title_string
-	
-	
-func _input(event):
-	if !(event is InputEventKey ):
-		return
-	
-	if !event.is_pressed():
-		return
-		
-	match event.keycode:
-		KEY_ESCAPE:
-			get_tree().change_scene_to_file( "res://gds_ui/gds_ui_root.tscn" )
-		KEY_A:
-			change_font_color_blue()
-		KEY_S:
-			change_font_color_green()
-		KEY_D:
-			append_text()
-
+	pam.set_name( "Label" )
+	pam.add_split()
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_lf()
+	pam.add_action( "Change Color : Blue", 	Key.KEY_A, change_font_color_blue )
+	pam.add_action( "Change Color : Green",	Key.KEY_S, change_font_color_green )
+	pam.add_action( "Append Text", 			Key.KEY_D, append_text )
+	build_summary( eSceneType.TEST )
 
 
 func change_font_color_blue():
-	get_node( "Summury" ).add_theme_color_override( "font_color", Color( 0, 0, 1 ) )
+	get_node( "Summary" ).add_theme_color_override( "font_color", Color( 0, 0, 1 ) )
 
 
 func change_font_color_green():
-	get_node( "Summury" ).set( "theme_override_colors/font_color", Color( 0, 1, 0 ) )
+	get_node( "Summary" ).set( "theme_override_colors/font_color", Color( 0, 1, 0 ) )
 
 
 func append_text():
-	get_node( "Summury" ).text += "\nTest String"
+	get_node( "Summary" ).text += "\nTest String"
