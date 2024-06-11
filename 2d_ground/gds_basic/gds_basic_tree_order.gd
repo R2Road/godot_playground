cf2adf941171402e6930737416ab19978499cc3c
R2Road <studior2road@gmail.com>
2024-06-11 22:35:04 +0900

    apply

--- 2d_ground/gds_basic/gds_basic_tree_order.gd
+++ 2d_ground/gds_basic/gds_basic_tree_order.gd
@@ -1,28 +1,28 @@
-extends GDSBasic_TreeOrder_Helper
+extends PlaygroundScene
 
 
 
 ############################ Override ############################
+func _init():
+	super._init()
+	print( name + " init" )
+
+
 func _ready():
-	set_process_input( true ) # is not need : default on
-
-	var title_string = \
-			"+ Tree Order" \
-			+ "\n" \
-			+ "[ESC] Return to Root" \
-			+ ""
-		
-	var summury_node = get_node( "Summury" )
-	summury_node.text = title_string
-
-
-func _input(event):
-	if !(event is InputEventKey ):
-		return
+	pam.set_name( "Tree Order" )
+	pam.add_lf()
+	pam.add_back( Key.KEY_ESCAPE )
+	build_summary( eSceneType.TEST )
 	
-	if !event.is_pressed():
-		return
-		
-	match event.keycode:
-		KEY_ESCAPE:
-			get_tree().change_scene_to_file("res://gds_basic/gds_basic_root.tscn")
+	print( name + " ready" )
+
+
+func _enter_tree():
+	print( name + " enter tree" )
+
+
+var test = true
+func _process( _delta ):
+	if test:
+		print( name + " process" )
+		test = false
