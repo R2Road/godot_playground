a5316428572d622a4899077f46be2fa52821d7ac
R2Road <studior2road@gmail.com>
2024-06-11 18:22:16 +0900

    set key map

--- 2d_ground/gds_sprite/gds_sprite_root.gd
+++ 2d_ground/gds_sprite/gds_sprite_root.gd
@@ -6,5 +6,7 @@ extends PlaygroundScene
 func _ready():
 	pam.set_name( "Sprite" )
 	pam.add_split()
-	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
+	pam.add_mover( "Playground",	Key.KEY_ESCAPE, "res://playground_main.tscn" )
+	pam.add_lf()
+	pam.add_mover( "Basic",			Key.KEY_1, "res://gds_sprite/gds_sprite_basic.tscn" )
 	build_summary( eSceneType.ROOT )
