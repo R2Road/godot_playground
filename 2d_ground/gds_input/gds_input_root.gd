73d5188f14b61435c58b46d617350f253d288ebc
R2Road <studior2road@gmail.com>
2024-06-11 11:28:06 +0900

    update script

--- 2d_ground/gds_input/gds_input_root.gd
+++ 2d_ground/gds_input/gds_input_root.gd
@@ -6,4 +6,6 @@ extends PlaygroundScene
 func _ready():
 	pam.set_name( "Input" )
 	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
+	pam.add_lf()
+	pam.add_mover( "Keyboard Input", 	Key.KEY_1, 		"res://gds_basic/gds_basic_input_keyboard.tscn" )
 	build_summary( eSceneType.ROOT )
