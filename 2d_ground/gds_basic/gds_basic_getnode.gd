d036874d819b3849d6f8600f374c3b157fb05824
R2Road <studior2road@gmail.com>
2024-06-11 10:27:34 +0900

    apply

--- 2d_ground/gds_basic/gds_basic_getnode.gd
+++ 2d_ground/gds_basic/gds_basic_getnode.gd
@@ -1,33 +1,18 @@
-extends Node2D
+extends PlaygroundScene
 
 
+
+############################ Override ############################
 func _ready():
-	set_process_input( true ) # is not need : default on
-
-	var title_string = \
-			"+ Get Node" \
-			+ "\n" \
-			+ "[ESC] Return to Root" \
-			+ ""
-		
-	var summury_node = get_node( "Summury" )
-	summury_node.text = title_string
+	pam.set_name( "Get Node" )
+	pam.add_back( Key.KEY_ESCAPE )
+	build_summary( eSceneType.TEST )
 	
 	DoPractice()
-	
-	
-func _input(event):
-	if !(event is InputEventKey ):
-		return
-	
-	if !event.is_pressed():
-		return
-		
-	match event.keycode:
-		KEY_ESCAPE:
-			get_tree().change_scene_to_file("res://gds_basic/gds_basic_root.tscn")
 
 
+
+############################   User   ############################
 func DoPractice():
 	get_node( "1" ).set_position( Vector2( 150, 150 ) )
 	get_node( "1/1_1" ).set_position( Vector2( 50, 50 ) )
