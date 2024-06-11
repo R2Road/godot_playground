828b8b58a3976ff02392837a985b7662b3bf5a0b
R2Road <studior2road@gmail.com>
2024-06-11 11:25:35 +0900

    rename

--- 2d_ground/gds_basic/gds_basic_root.gd
+++ 2d_ground/gds_basic/gds_basic_root.gd
@@ -9,7 +9,7 @@ func _ready():
 	pam.add_mover( "Empty",  			Key.KEY_1, 		"res://gds_basic/gds_basic_empty.tscn" )
 	pam.add_mover( "Dev Path",  		Key.KEY_2, 		"res://gds_basic/gds_basic_devpath.tscn" )
 	pam.add_mover( "Tree",  			Key.KEY_3, 		"res://gds_basic/gds_basic_tree.tscn" )
-	pam.add_mover( "OS", 				Key.KEY_4, 		"res://gds_basic/gds_basic_OSScene.tscn" )
+	pam.add_mover( "OS", 				Key.KEY_4, 		"res://gds_basic/gds_basic_os.tscn" )
 	pam.add_mover( "Tree Order", 		Key.KEY_5, 		"res://gds_basic/gds_basic_tree_order.tscn" )
 	pam.add_mover( "Log", 				Key.KEY_6, 		"res://gds_basic/gds_basic_log.tscn" )
 	pam.add_mover( "Keyboard Input", 	Key.KEY_7, 		"res://gds_basic/gds_basic_input_keyboard.tscn" )
