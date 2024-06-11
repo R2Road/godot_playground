097887c43b210a96737c7eb730a97e6822de79a8
R2Road <studior2road@gmail.com>
2024-06-11 11:14:38 +0900

    inline

--- 2d_ground/gds_basic/gds_basic_getnode.gd
+++ 2d_ground/gds_basic/gds_basic_getnode.gd
@@ -14,9 +14,32 @@ func _ready():
 
 ############################   User   ############################
 func DoPractice():
+	# 1
 	get_node( "1" ).set_position( Vector2( 150, 150 ) )
 	get_node( "1/1_1" ).set_position( Vector2( 50, 50 ) )
 	
+	
+	# 2
 	get_node( "2" ).set_position( Vector2( 350, 150 ) )
 	get_node( "2/2_1" ).set_position( Vector2( 50, 50 ) )
 	get_node( "2/2_1/2_1_1" ).set_position( Vector2( 50, 50 ) )
+	
+	
+	# 3 : 계층 거슬러 올라가기
+	var node_3_1_1 = get_node( "3/3_1/3_1_1" )
+	node_3_1_1.get_node( "../../../3" ).set_position( Vector2( 550, 150 ) )
+	
+	var label_3_1_1 = node_3_1_1.get_node( "../../../3/3_1/3_1_1" )
+	label_3_1_1.set_position( Vector2( 50, 50 ) )
+	
+	label_3_1_1.get_node( "../../3_1").set_position( Vector2( 50, 50 ) )
+	
+	
+	# 4
+	var node_4 = get_node( "4" )
+	node_4.set_position( Vector2( 750, 150 ) )
+	
+	var child_index = 1
+	for c in node_4.get_children():
+		c.set_position( Vector2( 50, 50 * child_index ) )
+		child_index += 1
