ed6142df4fbc94292d1c5b53277f6e0a0d1c4e8a
R2Road <studior2road@gmail.com>
2024-06-12 15:44:50 +0900

    remove warning

--- 2d_ground/base/playground_scene_helper.gd
+++ 2d_ground/base/playground_scene_helper.gd
@@ -7,5 +7,5 @@ func _ready():
 	self.position.y = get_viewport().size.y - self.get_minimum_size().y
 
 
-func _process( delta ):
+func _process( _delta ):
 	self.text = "fps : " + str( Engine.get_frames_per_second() )
