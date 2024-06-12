0c3457d6c0bd4a10894fb7abe2c4879be04e3da7
R2Road <studior2road@gmail.com>
2024-06-12 11:55:17 +0900

    apply

--- 2d_ground/gds_basic/gds_basic_preload_instantiate.gd
+++ 2d_ground/gds_basic/gds_basic_preload_instantiate.gd
@@ -1,8 +1,22 @@
-extends Node
+extends PlaygroundScene
 
-var test_sprite_node = preload( "res://gds_basic/gds_basic_preload_instantiate_helper.tscn" )
 
+
+############################ Variable ############################
+var test_node_resource = preload( "res://gds_basic/gds_basic_helper_sprite2d.tscn" )
+
+
+
+############################ Override ############################
 func _ready():
-	var tsn = test_sprite_node.instantiate()
-	tsn.position = Vector2( get_viewport().size.x * 0.5, get_viewport().size.y * 0.5 )
-	add_child( tsn )
+	pam.set_name( "Basic" )
+	pam.add_lf()
+	pam.add_back( Key.KEY_ESCAPE )
+	build_summary( eSceneType.TEST )
+	
+	#
+	# Start Practice
+	#
+	var test_node = test_node_resource.instantiate()
+	test_node.position = ( get_viewport().size * 0.5 )
+	add_child( test_node )
