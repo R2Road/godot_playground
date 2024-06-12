3b4e0bb5a9ba59e1db92e3276c034f224991310d
R2Road <studior2road@gmail.com>
2024-06-12 15:07:47 +0900

    update script

--- 2d_ground/gds_sprite/gds_sprite_load_texture.gd
+++ 2d_ground/gds_sprite/gds_sprite_load_texture.gd
@@ -12,13 +12,10 @@ func _ready():
 	#
 	# Start Practice
 	#
-	var sprite_resource_actor0001_idle_01 = load( "res://resources/sprite/actor001/actor001_idle_01.png" )
+	var loaded_texture = ResourceLoader.load( "res://resources/sprite/actor001/actor001_idle_01.png" )
 	
 	var sprite_node = Sprite2D.new()
-	sprite_node.texture = sprite_resource_actor0001_idle_01
-	sprite_node.position = Vector2( get_viewport().size.x * 0.5, get_viewport().size.y * 0.5 )
+	sprite_node.texture = loaded_texture
+	sprite_node.position = ( get_viewport().size * 0.5 )
 	sprite_node.scale = Vector2( 4, 4 )
-	#sprite_node.scale.x = 4
-	#sprite_node.scale.y = 4
-	
 	add_child( sprite_node )
