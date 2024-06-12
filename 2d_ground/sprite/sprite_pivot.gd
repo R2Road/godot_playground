9e0ebf6938812c50461aea312001759d790199da
R2Road <studior2road@gmail.com>
2024-06-12 18:22:17 +0900

    update script

--- 2d_ground/sprite/sprite_pivot.gd
+++ 2d_ground/sprite/sprite_pivot.gd
@@ -12,8 +12,9 @@ func _ready():
 	#
 	# Start Practice
 	#
-	var pivot_scene = ResourceLoader.load( "res://helper/helper_point.tscn" ).instantiate()
-	pivot_scene.position = get_viewport().size * 0.5
-	add_child( pivot_scene )
-	
 	$Sprite2D.set_position( get_viewport().size * 0.5 )
+	
+	# sprite2d position pivot
+	var pivot_scene = ResourceLoader.load( "res://helper/helper_point.tscn" ).instantiate()
+	pivot_scene.position = $Sprite2D.position
+	add_child( pivot_scene )
