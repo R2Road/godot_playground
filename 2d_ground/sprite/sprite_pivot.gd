0238883576f956bfa5b709247aefff2561beeecc
R2Road <studior2road@gmail.com>
2024-06-12 18:14:47 +0900

    use : RenderingServer.CANVAS_ITEM_Z_MAX

--- 2d_ground/sprite/sprite_pivot.gd
+++ 2d_ground/sprite/sprite_pivot.gd
@@ -14,7 +14,7 @@ func _ready():
 	#
 	var pivot_scene = ResourceLoader.load( "res://helper/helper_point.tscn" ).instantiate()
 	pivot_scene.position = get_viewport().size * 0.5
-	pivot_scene.z_index = 10
+	pivot_scene.z_index = RenderingServer.CANVAS_ITEM_Z_MAX
 	add_child( pivot_scene )
 	
 	$Sprite2D.set_position( get_viewport().size * 0.5 )
