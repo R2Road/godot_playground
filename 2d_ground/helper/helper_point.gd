03ead3ac107d20a81499f69414488251f8e0e965
R2Road <studior2road@gmail.com>
2024-06-12 18:16:45 +0900

    move

--- 2d_ground/helper/helper_point.gd
+++ 2d_ground/helper/helper_point.gd
@@ -3,5 +3,10 @@ extends Node2D
 
 
 ############################ Override ############################
+func _init():
+	self.set_z_index( RenderingServer.CANVAS_ITEM_Z_MAX )
+	#self.z_index = RenderingServer.CANVAS_ITEM_Z_MAX
+
+
 func _draw():
 	draw_circle( Vector2(), 1, Color( 0, 1, 0 ) )
