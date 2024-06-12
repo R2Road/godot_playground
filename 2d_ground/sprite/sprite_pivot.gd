03e6d505c99d3298419835a4850f09ad66bd2fd6
R2Road <studior2road@gmail.com>
2024-06-12 18:07:39 +0900

    add scene : sprite_pivot

--- 2d_ground/sprite/sprite_pivot.gd
+++ 2d_ground/sprite/sprite_pivot.gd
@@ -4,7 +4,7 @@ extends PlaygroundScene
 
 ############################ Override ############################
 func _ready():
-	pam.set_name( "Sprite Pivot" )
+	pam.set_name( "Pivot" )
 	pam.add_lf()
 	pam.add_back( Key.KEY_ESCAPE )
 	build_summary( eSceneType.TEST )
@@ -12,4 +12,9 @@ func _ready():
 	#
 	# Start Practice
 	#
+	var pivot_scene = ResourceLoader.load( "res://helper/helper_point.tscn" ).instantiate()
+	pivot_scene.position = get_viewport().size * 0.5
+	pivot_scene.z_index = 10
+	add_child( pivot_scene )
+	
 	$Sprite2D.set_position( get_viewport().size * 0.5 )
--- 2d_ground/sprite/sprite_root.gd
+++ 2d_ground/sprite/sprite_root.gd
@@ -4,10 +4,11 @@ extends PlaygroundScene
 
 ############################ Override ############################
 func _ready():
-	pam.set_name( "Sprite" )
+	pam.set_name( "Sprite Root" )
 	pam.add_split()
 	pam.add_mover( "Playground",	Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	pam.add_lf()
 	pam.add_mover( "Basic",			Key.KEY_1, "res://sprite/sprite_basic.tscn" )
 	pam.add_mover( "Load Texture",	Key.KEY_2, "res://sprite/sprite_load_texture.tscn" )
+	pam.add_mover( "Pivot",			Key.KEY_3, "res://sprite/sprite_pivot.tscn" )
 	build_summary( eSceneType.ROOT )
