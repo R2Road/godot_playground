78031c7d54135de02af588b0d59e92be949e0808
R2Road <studior2road@gmail.com>
2024-06-12 18:35:39 +0900

    update scene : sprite_pivot

--- 2d_ground/sprite/sprite_pivot.gd
+++ 2d_ground/sprite/sprite_pivot.gd
@@ -7,6 +7,11 @@ func _ready():
 	pam.set_name( "Pivot" )
 	pam.add_lf()
 	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_lf()
+	pam.add_action( "Move Offset : U", Key.KEY_S, move_offset_up )
+	pam.add_action( "Move Offset : D", Key.KEY_W, move_offset_down )
+	pam.add_action( "Move Offset : R", Key.KEY_D, move_offset_right )
+	pam.add_action( "Move Offset : L", Key.KEY_A, move_offset_left )
 	build_summary( eSceneType.TEST )
 	
 	#
@@ -18,3 +23,25 @@ func _ready():
 	var pivot_scene = ResourceLoader.load( "res://helper/helper_point.tscn" ).instantiate()
 	pivot_scene.position = $Sprite2D.position
 	add_child( pivot_scene )
+	
+	# Ready
+	update_info()
+
+
+
+############################   User   ############################
+func move_offset_up():
+	$Sprite2D.offset.y += 1
+	update_info()
+func move_offset_down():
+	$Sprite2D.offset.y -= 1
+	update_info()
+func move_offset_right():
+	$Sprite2D.offset.x += 1
+	update_info()
+func move_offset_left():
+	$Sprite2D.offset.x -= 1
+	update_info()
+
+func update_info():
+	$Sprite2D/Label.text = str( $Sprite2D.position ) + "\n" + str( $Sprite2D.offset )
