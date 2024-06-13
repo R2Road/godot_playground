2efa7e99f464fa4521b52c2cc9aeeeb8f1e1bd4c
R2Road <studior2road@gmail.com>
2024-06-13 14:31:44 +0900

    ...

--- 2d_ground/sprite/sprite_aliasing.gd
+++ 2d_ground/sprite/sprite_aliasing.gd
@@ -4,7 +4,7 @@ extends PlaygroundScene
 
 ############################ Override ############################
 func _ready():
-	pam.set_name( "Sprite Aliasing" )
+	pam.set_name( "Aliasing" )
 	pam.add_lf()
 	pam.add_back( Key.KEY_ESCAPE )
 	build_summary( eSceneType.TEST )
--- 2d_ground/sprite/sprite_basic.gd
+++ 2d_ground/sprite/sprite_basic.gd
@@ -4,7 +4,7 @@ extends PlaygroundScene
 
 ############################ Override ############################
 func _ready():
-	pam.set_name( "Sprite Pivot" )
+	pam.set_name( "Basic" )
 	pam.add_lf()
 	pam.add_back( Key.KEY_ESCAPE )
 	build_summary( eSceneType.TEST )
