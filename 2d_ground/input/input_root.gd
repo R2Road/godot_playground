7e50e42c2d4b5961068bb8c8b5ccb2a57b498483
R2Road <studior2road@gmail.com>
2024-06-14 00:46:42 +0900

    rename

--- 2d_ground/input/input_root.gd
+++ 2d_ground/input/input_root.gd
@@ -8,6 +8,6 @@ func _ready():
 	pam.add_split()
 	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	pam.add_lf()
-	pam.add_mover( "Keyboard Input", 	Key.KEY_1, 		"res://input/input_keyboard.tscn" )
+	pam.add_mover( "Keyboard Input : In Input", 	Key.KEY_1, 		"res://input/input_keyboard_in_input.tscn" )
 	pam.add_mover( "Keyboard Input : In Process", 	Key.KEY_2, 		"res://input/input_keyboard_in_process.tscn" )
 	build_summary( eSceneType.ROOT )
