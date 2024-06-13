2ae780b2277e1e49ebb59daf33bc002e05db57b4
R2Road <studior2road@gmail.com>
2024-06-13 15:17:01 +0900

    update scene

--- 2d_ground/sprite/sprite_animation_control.gd
+++ 2d_ground/sprite/sprite_animation_control.gd
@@ -8,9 +8,13 @@ func _ready():
 	pam.add_lf()
 	pam.add_back( Key.KEY_ESCAPE )
 	pam.add_lf()
-	pam.add_action( "Play Ani 1", Key.KEY_A, play_ani_1 )
-	pam.add_action( "Play Ani 2", Key.KEY_S, play_ani_2 )
-	pam.add_action( "Stop Ani", Key.KEY_D, stop_ani )
+	pam.add_action( "Stop Ani", Key.KEY_1, stop_ani )
+	pam.add_lf()
+	pam.add_action( "Play Ani 1", Key.KEY_Q, play_ani_1 )
+	pam.add_action( "Play Ani 2", Key.KEY_W, play_ani_2 )
+	pam.add_lf()
+	pam.add_action( "Play Once : Ani 1", Key.KEY_A, play_ani_1_once )
+	pam.add_action( "Play Once : Ani 2", Key.KEY_S, play_ani_2_once )
 	build_summary( eSceneType.TEST )
 	
 	#
@@ -22,8 +26,18 @@ func _ready():
 
 ############################   User   ############################
 func play_ani_1():
+	$AnimatedSprite2D.sprite_frames.set_animation_loop( "ani_1", true )
 	$AnimatedSprite2D.play( "ani_1" )
 func play_ani_2():
+	$AnimatedSprite2D.sprite_frames.set_animation_loop( "ani_2", true )
 	$AnimatedSprite2D.play( "ani_2" )
 func stop_ani():
 	$AnimatedSprite2D.stop()
+
+
+func play_ani_1_once():
+	$AnimatedSprite2D.sprite_frames.set_animation_loop( "ani_1", false )
+	$AnimatedSprite2D.play( "ani_1" )
+func play_ani_2_once():
+	$AnimatedSprite2D.sprite_frames.set_animation_loop( "ani_2", false )
+	$AnimatedSprite2D.play( "ani_2" )
