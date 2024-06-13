3c6684c7412c39395da8b3dd4b5b487c61e1acd8
R2Road <studior2road@gmail.com>
2024-06-13 18:20:09 +0900

    remove warning

--- 2d_ground/playground_exit.gd
+++ 2d_ground/playground_exit.gd
@@ -3,6 +3,6 @@ extends Node2D
 
 
 ############################ Override ############################
-func _process( delta ):
+func _process( _delta ):
 	set_process_input( false )
 	get_tree().quit()
