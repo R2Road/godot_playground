e608815fa52274fcc1439490631b20b0bca6e1b5
R2Road <studior2road@gmail.com>
2024-06-13 12:13:46 +0900

    add scene : sprite_aliasing

---
+++ 2d_ground/sprite/sprite_aliasing.gd
@@ -0,0 +1,20 @@
+extends PlaygroundScene
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( "Sprite Aliasing" )
+	pam.add_lf()
+	pam.add_back( Key.KEY_ESCAPE )
+	build_summary( eSceneType.TEST )
+	
+	#
+	# Start Practice
+	#
+	$Sprite2D_Default.set_position( get_viewport().size * 0.25 )
+	$Sprite2D_Aliasing_In_Editor.set_position( get_viewport().size * 0.5 )
+	$Sprite2D_Aliasing_In_Script.set_position( get_viewport().size * 0.75 )
+	
+	# Set Texture Filter
+	$Sprite2D_Aliasing_In_Script.set_texture_filter( CanvasItem.TEXTURE_FILTER_NEAREST )
--- 2d_ground/sprite/sprite_root.gd
+++ 2d_ground/sprite/sprite_root.gd
@@ -11,5 +11,6 @@ func _ready():
 	pam.add_mover( "Basic",			Key.KEY_1, "res://sprite/sprite_basic.tscn" )
 	pam.add_mover( "Load Texture",	Key.KEY_2, "res://sprite/sprite_load_texture.tscn" )
 	pam.add_mover( "Offset",		Key.KEY_3, "res://sprite/sprite_offset.tscn" )
-	pam.add_mover( "Animation",		Key.KEY_4, "res://sprite/sprite_animation.tscn" )
+	pam.add_mover( "Aliasing",		Key.KEY_4, "res://sprite/sprite_aliasing.tscn" )
+	pam.add_mover( "Animation",		Key.KEY_5, "res://sprite/sprite_animation.tscn" )
 	build_summary( eSceneType.ROOT )
