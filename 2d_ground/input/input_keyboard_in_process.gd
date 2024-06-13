2533afd085065e8e7c6592ec8f273327d64d71e1
R2Road <studior2road@gmail.com>
2024-06-14 00:45:29 +0900

    add scene : input_keyboard_in_process

---
+++ 2d_ground/input/input_keyboard_in_process.gd
@@ -0,0 +1,31 @@
+extends PlaygroundScene
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( "Keyboard Input : In Process" )
+	pam.add_back( Key.KEY_ESCAPE )
+	build_summary( eSceneType.TEST )
+	
+	#
+	# Start Practice
+	#
+	UpdateMessage( "-" )
+
+
+func _process( _delta ):
+	if Input.is_key_pressed( KEY_Q ):
+		UpdateMessage( "Q" )
+	if Input.is_key_pressed( KEY_W ):
+		UpdateMessage( "W" )
+
+
+############################   User   ############################
+func UpdateMessage( key_name : String ):
+	var message_node = get_node( "Message" )
+	message_node.text = "key pressed : " + key_name
+	message_node.set_position(
+			( get_viewport().size * 0.5 )
+			- ( message_node.get_minimum_size() * message_node.get_scale() * 0.5 )
+	)
--- 2d_ground/input/input_root.gd
+++ 2d_ground/input/input_root.gd
@@ -9,4 +9,5 @@ func _ready():
 	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	pam.add_lf()
 	pam.add_mover( "Keyboard Input", 	Key.KEY_1, 		"res://input/input_keyboard.tscn" )
+	pam.add_mover( "Keyboard Input : In Process", 	Key.KEY_2, 		"res://input/input_keyboard_in_process.tscn" )
 	build_summary( eSceneType.ROOT )
