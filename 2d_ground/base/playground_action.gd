2b37045bd1b0f496d5d21c29b788c31fb4059433
R2Road <studior2road@gmail.com>
2024-06-14 00:11:09 +0900

    update script

--- 2d_ground/base/playground_action.gd
+++ 2d_ground/base/playground_action.gd
@@ -17,6 +17,15 @@ static func new_exit( _owner : Node )->PlayGroundAction:
 	ret.name = "Exit"
 	ret.key = Key.KEY_ESCAPE
 	ret.action = func ():
+		#
+		# Scene 전환이 이미 일어났는지 확인
+		# PlaygroundAction은 call_deferred 로 작동한다.
+		# 키를 동시에 2개 이상 누른 경우
+		# 한 프레임에 여러번의 Scene 전환 가능
+		#
+		if _owner != _owner.get_tree().current_scene:
+			return
+		
 		_owner.get_tree().change_scene_to_file( "res://playground_exit.tscn" )
 	
 	return ret
@@ -28,6 +37,15 @@ static func new_mover( _owner : Node, _name : String, _key : Key, _next_scene_pa
 	ret.name = _name
 	ret.key = _key
 	ret.action = func ():
+		#
+		# Scene 전환이 이미 일어났는지 확인
+		# PlaygroundAction은 call_deferred 로 작동한다.
+		# 키를 동시에 2개 이상 누른 경우
+		# 한 프레임에 여러번의 Scene 전환 가능
+		#
+		if _owner != _owner.get_tree().current_scene:
+			return
+		
 		#
 		# 현재 Scene의 모든 개체를 제거한다.
 		#
@@ -52,6 +70,10 @@ static func new_mover( _owner : Node, _name : String, _key : Key, _next_scene_pa
 		# change_scene_to_file 을 이용한 Scene전환 이후
 		# 이전 Scene이 남아 있는 경우가 있다.
 		tree.set_current_scene( next_scene )
+		
+		# 같은 instance id 가 1번 이상 출력되면
+		# 한 프레임에 scene 전환이 여러번 수행된 것이다.
+		# print( "Last Scene Instance ID : " + str( _owner.get_instance_id() ) )
 	
 	return ret
 
