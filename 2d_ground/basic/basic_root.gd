05ee4484b3a994e83645221b46b5de96b90c30de
R2Road <studior2road@gmail.com>
2024-06-14 00:47:15 +0900

    ...

--- 2d_ground/basic/basic_root.gd
+++ 2d_ground/basic/basic_root.gd
@@ -13,7 +13,7 @@ func _ready():
 	pam.add_mover( "Tree",  			Key.KEY_3, 		"res://basic/basic_tree.tscn" )
 	pam.add_mover( "Tree Order", 		Key.KEY_4, 		"res://basic/basic_tree_order.tscn" )
 	pam.add_mover( "Log", 				Key.KEY_5, 		"res://basic/basic_log.tscn" )
-	pam.add_mover( "Keyboard Input", 	Key.KEY_6, 		"res://input/input_keyboard_in_input.tscn" )
+	pam.add_mover( "Input", 			Key.KEY_6, 		"res://input/input_keyboard_in_input.tscn" )
 	pam.add_mover( "Get Node", 			Key.KEY_7, 		"res://basic/basic_getnode.tscn" )
 	pam.add_lf()
 	pam.add_mover( "Change Scene",		Key.KEY_Q,	"res://basic/basic_change_scene.tscn" )
