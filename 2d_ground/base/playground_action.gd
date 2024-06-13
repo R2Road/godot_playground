facf9c51e12853dcd778fab6577c0defdb852e82
R2Road <studior2road@gmail.com>
2024-06-13 23:51:18 +0900

    update script : deferred call 을 초기화 할 방법을 모르겠다. 잠재적인 문제가 될 것이므로 queue_free 를 이용한 코드로 돌아간다.

--- 2d_ground/base/playground_action.gd
+++ 2d_ground/base/playground_action.gd
@@ -17,16 +17,6 @@ static func new_exit( _owner : Node )->PlayGroundAction:
 	ret.name = "Exit"
 	ret.key = Key.KEY_ESCAPE
 	ret.action = func ():
-		#
-		# PlaygroundAction은 call_deferred 로 작동한다.
-		# _input을 통해 한 프레임에 여러개의 PlaygroundAction이 deferred queue에 등록 가능 하다.
-		# Scene 전환 과정에서 owner node 는 free 된다.
-		# 이후에도 deferred queue에 PlaygroundAction이 남아 있을 수 있다.
-		# _owner를 검사해서 free 되었다면 action을 수행하지 않는다.
-		#
-		if null == _owner:
-			return
-		
 		_owner.get_tree().change_scene_to_file( "res://playground_exit.tscn" )
 	
 	return ret
@@ -39,14 +29,11 @@ static func new_mover( _owner : Node, _name : String, _key : Key, _next_scene_pa
 	ret.key = _key
 	ret.action = func ():
 		#
-		# PlaygroundAction은 call_deferred 로 작동한다.
-		# _input을 통해 한 프레임에 여러개의 PlaygroundAction이 deferred queue에 등록 가능 하다.
-		# Scene 전환 과정에서 owner node 는 free 된다.
-		# 이후에도 deferred queue에 PlaygroundAction이 남아 있을 수 있다.
-		# _owner를 검사해서 free 되었다면 action을 수행하지 않는다.
+		# 현재 Scene의 모든 개체를 제거한다.
 		#
-		if null == _owner:
-			return
+		var tree = _owner.get_tree()
+		for c in tree.root.get_children():
+			c.queue_free()
 		
 		#
 		# SceneTree 의 change scene 을 사용하지 않고 scene 전환 효과를 만든다.
@@ -56,13 +43,6 @@ static func new_mover( _owner : Node, _name : String, _key : Key, _next_scene_pa
 			next_scene.pam.set_last_scene( _owner.pam.name, _owner.scene_file_path )
 		else:
 			print_debug( "warning : is not PlayGroundScene" )
-		
-		#
-		# 현재 Scene의 모든 개체를 제거한다.
-		#
-		var tree = _owner.get_tree()
-		for c in tree.root.get_children():
-			c.free()
 		
 		#
 		# Scene 설정
@@ -76,23 +56,12 @@ static func new_mover( _owner : Node, _name : String, _key : Key, _next_scene_pa
 	return ret
 
 
-static func new_action( _owner : Node, _name : String, _key : Key, functor : Callable )->PlayGroundAction:
+static func new_action( _name : String, _key : Key, _functor : Callable )->PlayGroundAction:
 	var ret = PlayGroundAction.new()
 	
 	ret.name = _name
 	ret.key = _key	
-	ret.action = func():
-		#
-		# PlaygroundAction은 call_deferred 로 작동한다.
-		# _input을 통해 한 프레임에 여러개의 PlaygroundAction이 deferred queue에 등록 가능 하다.
-		# Scene 전환 과정에서 owner node 는 free 된다.
-		# 이후에도 deferred queue에 PlaygroundAction이 남아 있을 수 있다.
-		# _owner를 검사해서 free 되었다면 action을 수행하지 않는다.
-		#
-		if null == _owner:
-			return
-		
-		functor.call()
+	ret.action = _functor
 	
 	return ret
 
