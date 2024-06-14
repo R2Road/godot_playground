7e0d53d26e10e249fb8910a9dd6e9b0d7e1bee9a
R2Road <studior2road@gmail.com>
2024-06-14 20:03:29 +0900

    rename

--- 2d_ground/input/input_root.gd
+++ 2d_ground/input/input_root.gd
@@ -12,7 +12,7 @@ func _ready():
 	pam.add_mover( "Keyboard Input : In Process", 		Key.KEY_2, 		"res://input/input_keyboard_in_process.tscn" )
 	pam.add_lf()
 	pam.add_mover( "Input Map : In Project Setting",	Key.KEY_Q, 		"res://input/input_map_in_project_setting.tscn" )
-	pam.add_mover( "Input Map : Actions",				Key.KEY_W, 		"res://input/input_map_actions.tscn" )
+	pam.add_mover( "Input Map : Show Actions",			Key.KEY_W, 		"res://input/input_map_show_actions.tscn" )
 	pam.add_mover( "Input Map : Update Action Event",	Key.KEY_E, 		"res://input/input_map_update_action_event.tscn" )
 	pam.add_mover( "Input Map : Update Action",			Key.KEY_R, 		"res://input/input_map_update_action.tscn" )
 	build_summary( eSceneType.ROOT )
