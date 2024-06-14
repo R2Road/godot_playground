3953b65225a11217b88af2ddf68a2a9384eb141e
R2Road <studior2road@gmail.com>
2024-06-14 14:32:51 +0900

    add scene : input_map

---
+++ 2d_ground/input/input_map.gd
@@ -0,0 +1,44 @@
+extends PlaygroundScene
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( "Map" )
+	pam.add_lf()
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_lf()
+	pam.add_message( "[ W | A | S | D | SPACE ] Press" )
+	build_summary( eSceneType.TEST )
+	
+	#
+	# Start Practice
+	#
+	update_message( "Press Keyboard" )
+
+
+func _process( _delta ):
+	var text : String = ""
+	if Input.is_action_pressed( "test_w" ):
+		text += "w" + " "
+	if Input.is_action_pressed( "test_a" ):
+		text += "a" + " "
+	if Input.is_action_pressed( "test_s" ):
+		text += "s" + " "
+	if Input.is_action_pressed( "test_d" ):
+		text += "d" + " "
+	if Input.is_action_pressed( "test_space" ):
+		text += "space" + " "
+	
+	if not text.is_empty():
+		update_message( text )
+
+
+############################   User   ############################
+func update_message( text ):
+	var message_node = get_node( "Message" )
+	message_node.text = text
+	message_node.set_position(
+			( get_viewport().size * 0.5 )
+			- ( message_node.get_minimum_size() * message_node.get_scale() * 0.5 )
+	)
--- 2d_ground/input/input_root.gd
+++ 2d_ground/input/input_root.gd
@@ -10,4 +10,5 @@ func _ready():
 	pam.add_lf()
 	pam.add_mover( "Keyboard Input : In Input", 	Key.KEY_1, 		"res://input/input_keyboard_in_input.tscn" )
 	pam.add_mover( "Keyboard Input : In Process", 	Key.KEY_2, 		"res://input/input_keyboard_in_process.tscn" )
+	pam.add_mover( "Map", 							Key.KEY_3, 		"res://input/input_map.tscn" )
 	build_summary( eSceneType.ROOT )
