27e082511ab4e81e9400a0c7053a340ad7fc9c79
R2Road <studior2road@gmail.com>
2024-06-14 15:22:17 +0900

    ...

--- 2d_ground/input/input_root.gd
+++ 2d_ground/input/input_root.gd
@@ -4,7 +4,7 @@ extends PlaygroundScene
 
 ############################ Override ############################
 func _ready():
-	pam.set_name( "Input" )
+	pam.set_name( "Input Root" )
 	pam.add_split()
 	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	pam.add_lf()
