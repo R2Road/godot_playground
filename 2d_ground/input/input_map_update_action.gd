94e59a585e0437f6656144bdbfb22d76c43b8f66
R2Road <studior2road@gmail.com>
2024-06-14 19:54:41 +0900

    add scene : input_map_update_action

---
+++ 2d_ground/input/input_map_update_action.gd
@@ -0,0 +1,97 @@
+extends PlaygroundScene
+
+
+
+############################ Variable ############################
+var my_input_action_infos : Array
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( "Input Map : Update Action" )
+	pam.add_lf()
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_lf()
+	pam.add_message( "     > Action Changed : test_space > test_q" )
+	build_summary( eSceneType.TEST )
+	
+	#
+	# Start Practice
+	#	
+	key_change_4_test_space_action()
+	
+	collect_my_input_action_names()
+	show_my_input_action_infos()
+	
+	update_message( "Press Keyboard" )
+
+
+func _process( _delta ):
+	var text : String = ""
+	
+	for a in my_input_action_infos:
+		if Input.is_action_pressed( a[0] ):
+			text += OS.get_keycode_string( a[1] ) + " "	
+	
+	if not text.is_empty():
+		update_message( text )
+
+
+func _exit_tree():
+	rollback_key_test_space_action()	
+
+
+
+############################   User   ############################
+func update_message( text ):
+	var message_node = get_node( "Message" )
+	message_node.text = text
+	message_node.set_position(
+			Vector2( get_viewport().size.x * 0.3, get_viewport().size.y * 0.5 )
+			- ( message_node.get_minimum_size() * message_node.get_scale() * 0.5 )
+	)
+
+
+func key_change_4_test_space_action():
+	InputMap.erase_action( "test_space" )
+	
+	InputMap.add_action( "test_q" )
+	var new_input_event_key = InputEventKey.new()
+	new_input_event_key.physical_keycode = KEY_Q
+	InputMap.action_add_event( "test_q", new_input_event_key )
+
+
+func rollback_key_test_space_action():
+	InputMap.erase_action( "test_q" )
+	
+	InputMap.add_action( "test_space" )	
+	var new_input_event_key = InputEventKey.new()
+	new_input_event_key.physical_keycode = KEY_SPACE
+	InputMap.action_add_event( "test_space", new_input_event_key )
+
+
+func collect_my_input_action_names():
+	for a in InputMap.get_actions():
+		if not a.contains( "test" ):
+			continue
+			
+		my_input_action_infos.push_back( [
+			a
+			, InputMap.action_get_events( a )[0].physical_keycode
+		] )
+
+
+func show_my_input_action_infos():
+	var s : String = ""
+	for a in my_input_action_infos:
+		s += a[0] + " : "
+		s += " " + OS.get_keycode_string( a[1] )
+		s += "\n"
+	
+	var label = get_node( "Actions" )
+	label.text = s
+	label.set_position(
+		Vector2( get_viewport().size.x * 0.7, get_viewport().size.y * 0.5 )
+		- ( label.get_minimum_size() * label.get_scale() * 0.5 )
+	)
--- 2d_ground/input/input_root.gd
+++ 2d_ground/input/input_root.gd
@@ -14,4 +14,5 @@ func _ready():
 	pam.add_mover( "Input Map : In Project Setting",	Key.KEY_Q, 		"res://input/input_map_in_project_setting.tscn" )
 	pam.add_mover( "Input Map : Actions",				Key.KEY_W, 		"res://input/input_map_actions.tscn" )
 	pam.add_mover( "Input Map : Update Action Event",	Key.KEY_E, 		"res://input/input_map_update_action_event.tscn" )
+	pam.add_mover( "Input Map : Update Action",			Key.KEY_R, 		"res://input/input_map_update_action.tscn" )
 	build_summary( eSceneType.ROOT )
