6ceaa2849e1e4178f6358b639606f92b5b162e00
R2Road <studior2road@gmail.com>
2024-06-17 14:25:18 +0900

    update script : basic_os

--- 2d_ground/basic/basic_os.gd
+++ 2d_ground/basic/basic_os.gd
@@ -7,15 +7,21 @@ func _ready():
 	pam.set_name( "OS" )
 	pam.add_back( Key.KEY_ESCAPE )
 	pam.add_lf()
-	pam.add_action( "OS.shell_open( url )",  Key.KEY_1, test_shell_open )
-	pam.add_action( "Time.get_datetime_dict_from_system()",  Key.KEY_2, test_get_dates )
+	pam.add_action( "OS.shell_open( url )",  Key.KEY_1, test_shell_open_url )
+	pam.add_action( "OS.shell_open( dir )",  Key.KEY_2, test_shell_open_dir )
+	pam.add_action( "Time.get_datetime_dict_from_system()",  Key.KEY_3, test_get_dates )
 	build_summary( eSceneType.TEST )
 
 
 
 ############################   User   ############################
-func test_shell_open():
+func test_shell_open_url():
 	UpdateMessage( OS.shell_open("https://godotengine.org/") )
+
+
+func test_shell_open_dir():
+	OS.shell_open( "C://" )
+	UpdateMessage( "OS.shell_open( \"C://\" )" )
 
 
 func test_get_dates():
