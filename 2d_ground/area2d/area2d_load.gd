1ceffac3fe9d61f479c50c43d9986271533168f2
R2Road <studior2road@gmail.com>
2024-06-18 17:30:46 +0900

    update scene : area2d_load

--- 2d_ground/area2d/area2d_load.gd
+++ 2d_ground/area2d/area2d_load.gd
@@ -18,9 +18,37 @@ func _ready():
 	#
 	# Start Practice
 	#
-	var shape = RectangleShape2D.new()
-	shape.size = Vector2( 134, 216 )
-	$Area2D_Area_Load_Shape_In_Script/CollisionShape2D.shape = shape
+	
+	# Area2D_Area_Load_Shape_In_Script
+	$Area2D_Area_Load_Shape_In_Script/CollisionShape2D.shape = RectangleShape2D.new()
+	$Area2D_Area_Load_Shape_In_Script/CollisionShape2D.shape.size = Vector2( 134, 216 )
+	
+	
+	# Area2D_Load_In_Script
+	var area2d_load_in_script = Area2D.new()
+	area2d_load_in_script.name = "Area2D_Load_In_Script"
+	add_child( area2d_load_in_script )
+	$Area2D_Load_In_Script.set_position( $Area2D_Area_Load_Shape_In_Script.position + Vector2( 350, 0 ) )
+	
+	var cs2d = CollisionShape2D.new()
+	cs2d.name = "CollisionShape2D"
+	$Area2D_Load_In_Script.add_child( cs2d )
+	$Area2D_Load_In_Script/CollisionShape2D.shape = RectangleShape2D.new()
+	$Area2D_Load_In_Script/CollisionShape2D.shape.size = Vector2( 134, 216 )
+	
+	var sprite = Sprite2D.new()
+	sprite.name = "Sprite2D"
+	$Area2D_Load_In_Script.add_child( sprite )
+	$Area2D_Load_In_Script/Sprite2D.texture = ResourceLoader.load( "res://resources/sprite/actor001/actor001_idle_01.png" )
+	$Area2D_Load_In_Script/Sprite2D.set_texture_filter( CanvasItem.TEXTURE_FILTER_NEAREST )
+	$Area2D_Load_In_Script/Sprite2D.scale = Vector2( 4, 4 )
+	
+	var label = Label.new()
+	label.name = "Label"
+	$Area2D_Load_In_Script.add_child( label )
+	$Area2D_Load_In_Script/Label.text = "Sensor\nLoad In Script"
+	$Area2D_Load_In_Script/Label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	$Area2D_Load_In_Script/Label.set_position( Vector2( -52, 76 ) )
 
 
 func _physics_process( delta ):
