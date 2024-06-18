cf076c9f4b62b5853d1411f2b3c14acb164c0562
R2Road <studior2road@gmail.com>
2024-06-18 17:04:10 +0900

    add scene : area2d_load

---
+++ 2d_ground/area2d/area2d_load.gd
@@ -0,0 +1,47 @@
+extends PlaygroundScene
+
+
+
+############################ Variable ############################
+const move_speed : int = 200
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( "Area2d : Load" )
+	pam.add_split()
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_lf()
+	pam.add_message( "[ W | A | S | D ] Move Player" )
+	build_summary( eSceneType.TEST )
+	
+	#
+	# Start Practice
+	#
+	var shape = RectangleShape2D.new()
+	shape.size = Vector2( 134, 216 )
+	$Area2D_Area_Load_Shape_In_Script/CollisionShape2D.shape = shape
+
+
+func _physics_process( delta ):
+	var move_vec : Vector2 = Vector2.ZERO
+	
+	if Input.is_key_pressed( KEY_W ):
+		move_vec.y -= 1
+	if Input.is_key_pressed( KEY_S ):
+		move_vec.y += 1
+	if Input.is_key_pressed( KEY_A ):
+		move_vec.x -= 1
+	if Input.is_key_pressed( KEY_D ):
+		move_vec.x += 1
+		
+	if 0 != move_vec.x or 0 != move_vec.y:
+		$Area2D_Player.position += ( move_vec * move_speed * delta )
+
+
+func _on_area_2d_player_area_entered(area):
+	$Label.text = "O"
+
+
+func _on_area_2d_player_area_exited(area):
+	$Label.text = "X"
--- 2d_ground/area2d/area2d_root.gd
+++ 2d_ground/area2d/area2d_root.gd
@@ -9,4 +9,5 @@ func _ready():
 	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	pam.add_lf()
 	pam.add_mover( "Basic",	Key.KEY_1, "res://area2d/area2d_basic.tscn" )
+	pam.add_mover( "Load",	Key.KEY_2, "res://area2d/area2d_load.tscn" )
 	build_summary( eSceneType.ROOT )
