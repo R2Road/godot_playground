941dab50b89cefaffa552ed3d6d66d1fa527d755
R2Road <studior2road@gmail.com>
2024-06-19 14:38:41 +0900

    add scene : etc_timer

--- 2d_ground/etc/etc_root.gd
+++ 2d_ground/etc/etc_root.gd
@@ -9,4 +9,5 @@ func _ready():
 	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	pam.add_lf()
 	pam.add_mover( "Click Sprite",		Key.KEY_1, 		"res://etc/etc_click_sprite.tscn" )
+	pam.add_mover( "Timer",				Key.KEY_2, 		"res://etc/etc_timer.tscn" )
 	build_summary( eSceneType.ROOT )
---
+++ 2d_ground/etc/etc_timer.gd
@@ -0,0 +1,57 @@
+extends PlaygroundScene
+
+
+
+############################ Variable ############################
+var timeout_count_1 : int = 0
+var timeout_count_2 : int = 0
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( "Timer" )
+	pam.add_split()
+	pam.add_back( Key.KEY_ESCAPE )
+	build_summary( eSceneType.TEST )
+	
+	#
+	# Start Practice
+	#
+	var timer = Timer.new()
+	timer.name = "Timer_2"
+	add_child( timer )
+	
+	$Timer_2.wait_time = 1
+	$Timer_2.timeout.connect( _on_timer_timeout_2 )
+	$Timer_2.start()
+	
+	show_timeout_info()
+
+
+
+############################  Signal  ############################
+func _on_timer_timeout():
+	timeout_count_1 += 1
+	show_timeout_info()
+
+func _on_timer_timeout_2():
+	timeout_count_2 += 1
+	show_timeout_info()
+	
+	$Timer_2.start()
+
+
+
+############################   User   ############################
+func show_timeout_info():
+	var s : String = ""
+	s += "time out count 1 : " + str( timeout_count_1 )
+	s += "\n"
+	s += "time out count 2 : " + str( timeout_count_2 )
+	
+	$Label.text = s
+	$Label.set_position(
+		( get_viewport().size * 0.5 )
+		- ( $Label.get_minimum_size() * $Label.get_scale() * 0.5 )
+	)
