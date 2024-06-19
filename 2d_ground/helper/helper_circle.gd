6854606af5be1ecf969787d3e254e2a5b1f38372
R2Road <studior2road@gmail.com>
2024-06-19 17:29:23 +0900

    split scene : helper_point, helper_circle

--- 2d_ground/helper/helper_circle.gd
+++ 2d_ground/helper/helper_circle.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 
 ############################ Variable ############################
+@export var offset : Vector2 = Vector2( 0, 0 )
 @export var color = Color( 0, 1, 0 )
+@export var radius : float = 1
 
 
 
@@ -14,4 +16,4 @@ func _init():
 
 
 func _draw():
-	draw_circle( Vector2( 0, 0 ), 1, color )
+	draw_circle( offset, radius, color )
