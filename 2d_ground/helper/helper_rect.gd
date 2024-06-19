1bd660613cd601bcd17d329cea8b80b690b2887f
R2Road <studior2road@gmail.com>
2024-06-19 17:05:36 +0900

    add helper : helper_rect

---
+++ 2d_ground/helper/helper_rect.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+
+@export var rect : Rect2 = Rect2( 0, 0, 10, 10 )
+@export var color = Color( 0, 1, 0 )
+
+############################ Override ############################
+func _init():
+	self.set_z_index( RenderingServer.CANVAS_ITEM_Z_MAX )
+	#self.z_index = RenderingServer.CANVAS_ITEM_Z_MAX
+
+
+func _draw():
+	draw_rect( rect, color )
