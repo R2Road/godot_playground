5376e698eb033a2bbb803e9debd8aea218769502
R2Road <studior2road@gmail.com>
2024-06-19 17:08:37 +0900

    update scene

--- 2d_ground/helper/helper_rect.gd
+++ 2d_ground/helper/helper_rect.gd
@@ -4,6 +4,8 @@ extends Node2D
 
 @export var rect : Rect2 = Rect2( 0, 0, 10, 10 )
 @export var color = Color( 0, 1, 0 )
+@export var fill = true
+@export var out_line_bold = 1.0
 
 ############################ Override ############################
 func _init():
@@ -12,4 +14,4 @@ func _init():
 
 
 func _draw():
-	draw_rect( rect, color )
+	draw_rect( rect, color, fill, out_line_bold )
