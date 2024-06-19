eeb0bb8d021473797df74f667eda9be3a2ab7d06
R2Road <studior2road@gmail.com>
2024-06-19 17:15:48 +0900

    update scene

--- 2d_ground/helper/helper_point.gd
+++ 2d_ground/helper/helper_point.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 
 
+############################ Variable ############################
+@export var offset : Vector2 = Vector2( 0, 0 )
+@export var color = Color( 0, 1, 0 )
+@export var radius : float = 1
+
+
+
 ############################ Override ############################
 func _init():
 	self.set_z_index( RenderingServer.CANVAS_ITEM_Z_MAX )
@@ -9,4 +16,4 @@ func _init():
 
 
 func _draw():
-	draw_circle( Vector2(), 1, Color( 0, 1, 0 ) )
+	draw_circle( offset, radius, color )
