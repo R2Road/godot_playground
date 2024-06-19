754af6aeadf205eb175d3efd5ef855f0c608d3ea
R2Road <studior2road@gmail.com>
2024-06-19 17:05:20 +0900

    add scene : helper_root

--- 2d_ground/playground_main.gd
+++ 2d_ground/playground_main.gd
@@ -18,6 +18,7 @@ func _ready():
 	pam.add_mover( "Json",		Key.KEY_Q, 		"res://json/json_root.tscn" )
 	pam.add_lf()
 	pam.add_mover( "ETC",		Key.KEY_Z, 		"res://etc/etc_root.tscn" )
+	pam.add_mover( "Helper",	Key.KEY_X, 		"res://helper/helper_root.tscn" )
 	pam.add_split()
 	pam.add_mover( "Test change scene without PlaygroundScene",	Key.KEY_SPACE,	"res://dummy.tscn" )
 	build_summary( eSceneType.ROOT )
