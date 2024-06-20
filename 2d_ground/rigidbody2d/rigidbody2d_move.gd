5b8dee3b3a730fd025332ebcfba4afeb84473e40
R2Road <studior2road@gmail.com>
2024-06-20 15:29:27 +0900

    add scene : rigidbody2d_move

---
+++ 2d_ground/rigidbody2d/rigidbody2d_move.gd
@@ -0,0 +1,47 @@
+extends PlaygroundScene
+
+
+
+############################ Variable ############################
+@export var move_speed : int = 300
+@export var spin_speed : int = 1000
+
+var thrust = false
+var spin = 0
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( "RigidBody2D : Move" )
+	pam.add_split()
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_lf()
+	pam.add_message( "[ W | A | S | D ] Move Player" )
+	build_summary( eSceneType.TEST )
+
+
+func _unhandled_key_input( event ):
+	if KEY_W == event.keycode:
+		if event.is_pressed():
+			thrust = true
+		else:
+			thrust = false
+	
+	if KEY_A == event.keycode:
+		if event.is_pressed():
+			spin = -1
+		else:
+			spin = 0
+	
+	if KEY_D == event.keycode:
+		if event.is_pressed():
+			spin = 1
+		else:
+			spin = 0
+
+func _physics_process( delta ):
+	# Move Front
+	$Player.constant_force = ( ( $Player.transform.x * move_speed ) if thrust else Vector2.ZERO )
+	
+	# Spin
+	$Player.constant_torque = spin * spin_speed
--- 2d_ground/rigidbody2d/rigidbody2d_root.gd
+++ 2d_ground/rigidbody2d/rigidbody2d_root.gd
@@ -9,4 +9,5 @@ func _ready():
 	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	pam.add_lf()
 	pam.add_mover( "Basic",			Key.KEY_1, "res://rigidbody2d/rigidbody2d_basic.tscn" )
+	pam.add_mover( "Move",			Key.KEY_2, "res://rigidbody2d/rigidbody2d_move.tscn" )
 	build_summary( eSceneType.ROOT )
