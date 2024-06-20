8ec3a0fac6f5d3c5ea6412c24707a74c257251c5
R2Road <studior2road@gmail.com>
2024-06-20 12:21:29 +0900

    add scene : rigidbody2d_basic

--- 2d_ground/rigidbody2d/rigidbody2d_root.gd
+++ 2d_ground/rigidbody2d/rigidbody2d_root.gd
@@ -7,4 +7,6 @@ func _ready():
 	pam.set_name( "RigidBody2D Root" )
 	pam.add_split()
 	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
+	pam.add_lf()
+	pam.add_mover( "Basic",			Key.KEY_1, "res://rigidbody2d/rigidbody2d_basic.tscn" )
 	build_summary( eSceneType.ROOT )
