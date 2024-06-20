38840242590aeac83e94d5776fc0d79dc72726e9
R2Road <studior2road@gmail.com>
2024-06-20 16:39:03 +0900

    add scene : rigidbody2d_move_4way

---
+++ 2d_ground/rigidbody2d/rigidbody2d_move_4way.gd
@@ -0,0 +1,33 @@
+extends PlaygroundScene
+
+
+
+############################ Variable ############################
+@export var move_speed : int = 500
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( "RigidBody2D : Move 4-Way" )
+	pam.add_split()
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_lf()
+	pam.add_message( "[ W | A | S | D ] Move Player" )
+	build_summary( eSceneType.TEST )
+
+
+func _physics_process( delta ):
+	var move_vec : Vector2 = Vector2.ZERO
+	
+	if Input.is_key_pressed( KEY_W ):
+		move_vec.y -= 1
+	if Input.is_key_pressed( KEY_S ):
+		move_vec.y += 1
+	if Input.is_key_pressed( KEY_A ):
+		move_vec.x -= 1
+	if Input.is_key_pressed( KEY_D ):
+		move_vec.x += 1
+		
+	if 0 != move_vec.x or 0 != move_vec.y:
+		$Player.linear_velocity += ( move_vec * move_speed * delta )
