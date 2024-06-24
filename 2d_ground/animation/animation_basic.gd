134188b934ffd6b40fe07590fdef0aaf673b8cd0
R2Road <studior2road@gmail.com>
2024-06-24 17:56:59 +0900

    add scene : animation_basic

---
+++ 2d_ground/animation/animation_basic.gd
@@ -0,0 +1,10 @@
+extends PlaygroundScene
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( "Animation : Basic" )
+	pam.add_split()
+	pam.add_back( Key.KEY_ESCAPE )
+	build_summary( eSceneType.TEST )
