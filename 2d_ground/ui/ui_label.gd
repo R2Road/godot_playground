235f185d2b2c5926b29c8a61a8e0f30f01600adf
R2Road <studior2road@gmail.com>
2024-06-25 14:50:46 +0900

    update scene

--- 2d_ground/ui/ui_label.gd
+++ 2d_ground/ui/ui_label.gd
@@ -12,15 +12,23 @@ func _ready():
 	pam.add_action( "Change Color : Green",	Key.KEY_S, change_font_color_green )
 	pam.add_action( "Append Text", 			Key.KEY_D, append_text )
 	build_summary( eSceneType.TEST )
+	
+	#
+	# Start Practice
+	#
+	$Label.set_position(
+		( get_viewport().size * 0.5 )
+		- ( $Label.get_minimum_size() * $Label.get_scale() * 0.5 )
+	)
 
 
 func change_font_color_blue():
-	get_node( "Summary" ).add_theme_color_override( "font_color", Color( 0, 0, 1 ) )
+	$Label.add_theme_color_override( "font_color", Color( 0, 0, 1 ) )
 
 
 func change_font_color_green():
-	get_node( "Summary" ).set( "theme_override_colors/font_color", Color( 0, 1, 0 ) )
+	$Label.set( "theme_override_colors/font_color", Color( 0, 1, 0 ) )
 
 
 func append_text():
-	get_node( "Summary" ).text += "\nTest String"
+	$Label.text += "\nTest String"
