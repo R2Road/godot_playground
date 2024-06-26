463f27080726e80ce83e0b4b9539573ab300be4e
R2Road <studior2road@gmail.com>
2024-06-26 22:21:46 +0900

    ...

--- 2d_ground/camera2d/camera2d_root.gd
+++ 2d_ground/camera2d/camera2d_root.gd
@@ -4,7 +4,7 @@ extends PlaygroundScene
 
 ############################ Override ############################
 func _ready():
-	pam.set_name( "ETC Root" )
+	pam.set_name( "Camera2D Root" )
 	pam.add_split()
 	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	pam.add_lf()
