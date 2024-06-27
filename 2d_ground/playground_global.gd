7cc453eaac3afe1f9509889b4c2cc48016f46665
R2Road <studior2road@gmail.com>
2024-06-28 08:58:25 +0900

    ...

--- 2d_ground/playground_global.gd
+++ 2d_ground/playground_global.gd
@@ -7,6 +7,16 @@ var on_assert = true
 
 
 
+############################ Override ############################
+func _enter_tree():
+	# 호출 된다.
+	print( "PGG : Enter Tree" )
+func _exit_tree():
+	# 호출 안된다. ( 왜? )
+	print( "PGG : Exit Tree" )
+
+
+
 ############################   User   ############################
 func _assert( flag ):
 	if on_assert:
