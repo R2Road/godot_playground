5d5bbc0223e886132c470b64537235389a169eef
R2Road <studior2road@gmail.com>
2024-07-04 19:11:51 +0900

    add scene : tilemap_basic

--- 2d_ground/tilemap/tilemap_root.gd
+++ 2d_ground/tilemap/tilemap_root.gd
@@ -8,5 +8,6 @@ func _ready():
 	pam.add_split()
 	pam.add_mover( "Playground",		Key.KEY_ESCAPE, "res://playground_main.tscn" )
 	pam.add_lf()
+	pam.add_mover( "Basic", Key.KEY_1, "res://tilemap/tilemap_basic.tscn" )
 	pam.add_mover( "Demo", Key.KEY_SPACE, "res://tilemap/tilemap_demo_1.tscn" )
 	build_summary( eSceneType.ROOT )
