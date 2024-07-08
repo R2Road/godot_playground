6adaa54db6bc2b4db788cb85e1203d437c46d6b5
R2Road <studior2road@gmail.com>
2024-07-08 16:17:21 +0900

    update helper

--- 2d_ground/helper/helper_move2center.gd
+++ 2d_ground/helper/helper_move2center.gd
@@ -5,4 +5,11 @@ extends Node2D
 ############################ Override ############################
 func _ready():
 	var target_position = get_viewport().size * 0.5
-	get_parent().position = get_parent().get_parent().to_local( target_position )
+	
+	if get_parent() is Control:
+		get_parent().position = get_parent().get_parent().to_local(
+			target_position
+			 - ( get_parent().size * get_parent().scale * 0.5 )
+		)
+	else:
+		get_parent().position = get_parent().get_parent().to_local( target_position )
