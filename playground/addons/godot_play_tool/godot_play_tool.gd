1ce45d0e5460858b3e242f471d12fca875fde89b
R2Road <studior2road@gmail.com>
2024-07-13 18:00:42 +0900

    move + rename

--- playground/addons/godot_play_tool/godot_play_tool.gd
+++ playground/addons/godot_play_tool/godot_play_tool.gd
@@ -3,10 +3,21 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	# Initialization of the plugin goes here.
-	pass
+	#
+	# Editor가 열리는 과정에 로드 된다.
+	# Script를 수정해도 바로 반영되지 않는다.
+	# Editor 닫고 다시 열어라.
+	#
+	# add_autoload_singleton 함수는 Project 의 설정을 바꾼다.
+	#
+	# Singleton의 이름을 바꿨다면 이전 이름이 Project Setting에 남아 있을 수 있다.
+	# "Project Setting > Auto Load" 에서 확인해라.
+	#
+	add_autoload_singleton( 
+		"PGG"
+		, "res://addons/godot_play_tool/godot_play_tool_global.gd"
+	)
 
 
 func _exit_tree():
-	# Clean-up of the plugin goes here.
-	pass
+	remove_autoload_singleton( "PGG" )
