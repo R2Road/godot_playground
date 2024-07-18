b301f9f8218939d3186997dd2adfc828c4fae516
R2Road <studior2road@gmail.com>
2024-07-18 20:55:28 +0900

    update scene : input_mouse_position

--- playground/input/input_mouse_position.gd
+++ playground/input/input_mouse_position.gd
@@ -11,17 +11,27 @@ static func scene_name()->String:
 ############################ Override ############################
 func _ready():
 	pam.set_name( scene_name() )
-	pam.add_lf()
 	pam.add_back( Key.KEY_ESCAPE )
 	pam.add_lf()
-	pam.add_action( "Camera U", Key.KEY_W, func(): $Label.position.y -= 10 )
-	pam.add_action( "Camera D", Key.KEY_S, func(): $Label.position.y += 10 )
-	pam.add_action( "Camera L", Key.KEY_A, func(): $Label.position.x -= 10 )
-	pam.add_action( "Camera R", Key.KEY_D, func(): $Label.position.x += 10 )
+	pam.add_message( "> DisplayServer.mouse_get_position() : Windows의 마우스 좌표." )
+	pam.add_message( "> get_viewport().get_mouse_position() : Viewport 위의 좌표는 Camera 이동에 영향 받지 않는다." )
+	pam.add_message( "> CanvasItem.get_global_mouse_position() : CanvasItem이 제공하는 mouse의 월드 좌표." )
 	pam.add_lf()
-	pam.add_message( "> Viewport 위의 자표라서 Camera 이동에 영향 받지 않는다." )
+	pam.add_action( "Camera U", Key.KEY_W, func(): $helper_reticle.position.y -= 10 )
+	pam.add_action( "Camera D", Key.KEY_S, func(): $helper_reticle.position.y += 10 )
+	pam.add_action( "Camera L", Key.KEY_A, func(): $helper_reticle.position.x -= 10 )
+	pam.add_action( "Camera R", Key.KEY_D, func(): $helper_reticle.position.x += 10 )
+	pam.add_lf()
+	pam.add_message( "> 초록점이 0, 0" )
 	build_summary( eSceneType.TEST )
 
 
 func _process( _delta ):
-	$Label.text = "mouse : %s" % str( get_viewport().get_mouse_position() )
+	$Label.text = (
+		"mouse on display server : %s\n\nmouse on viewport : %s\n\n mouse on global : %s"
+		% [
+			str( DisplayServer.mouse_get_position() )
+			, str( get_viewport().get_mouse_position() )
+			, str( get_global_mouse_position() )
+		]
+	)
