1883730917ff6f6d2739e67de4e12fc16f4b238e
R2Road <studior2road@gmail.com>
2024-07-23 14:51:52 +0900

    add scene : ui_label_align

---
+++ playground/2d/ui/ui_label_align.gd
@@ -0,0 +1,39 @@
+class_name ui_label_align extends GDPTScene
+
+
+
+######################### GDPT Override ##########################
+static func scene_name()->String:
+	return "Label : Align"
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( ui_label_align.scene_name() )
+	pam.add_split()
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_lf()
+	pam.add_action( "H Alignment : L", 	Key.KEY_Q, func(): change_horizontal_alignment( HorizontalAlignment.HORIZONTAL_ALIGNMENT_LEFT ) )
+	pam.add_action( "H Alignment : C", 	Key.KEY_W, func(): change_horizontal_alignment( HorizontalAlignment.HORIZONTAL_ALIGNMENT_CENTER ) )
+	pam.add_action( "H Alignment : R", 	Key.KEY_E, func(): change_horizontal_alignment( HorizontalAlignment.HORIZONTAL_ALIGNMENT_RIGHT ) )
+	pam.add_lf()
+	pam.add_action( "V Alignment : L", 	Key.KEY_A, func(): change_vertical_alignment( VerticalAlignment.VERTICAL_ALIGNMENT_TOP ) )
+	pam.add_action( "V Alignment : C", 	Key.KEY_S, func(): change_vertical_alignment( VerticalAlignment.VERTICAL_ALIGNMENT_CENTER ) )
+	pam.add_action( "V Alignment : R", 	Key.KEY_D, func(): change_vertical_alignment( VerticalAlignment.VERTICAL_ALIGNMENT_BOTTOM ) )
+	pam.add_lf()
+	pam.add_message( "Alignment 속성은 Control>Layout>Transform 에 설정된 size의 범위에서 작동한다." )
+	build_summary( eSceneType.TEST )
+	
+	#
+	# Start Practice
+	#
+	$Label/helper_rect.rect.size = $Label.size
+
+
+func change_horizontal_alignment( a : HorizontalAlignment ):
+	$Label.horizontal_alignment = a
+
+
+func change_vertical_alignment( a : VerticalAlignment ):
+	$Label.vertical_alignment = a
--- playground/2d/ui/ui_root.gd
+++ playground/2d/ui/ui_root.gd
@@ -8,5 +8,6 @@ func _ready():
 	pam.add_split()
 	pam.add_mover( "2D Root",		Key.KEY_ESCAPE, "res://2d/2d_root.tscn" )
 	pam.add_lf()
-	pam.add_mover( "Label",  		Key.KEY_1, 		"res://2d/ui/ui_label.tscn" )
+	pam.add_mover( "Label",  					Key.KEY_1, 		"res://2d/ui/ui_label.tscn" )
+	pam.add_mover( ui_label_align.scene_name(), Key.KEY_2, 		"res://2d/ui/ui_label_align.tscn" )
 	build_summary( eSceneType.ROOT )
