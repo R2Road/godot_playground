74a93d9a3794fb7f1790f15dcaeffe1b582f2860
R2Road <studior2road@gmail.com>
2024-08-03 22:59:04 +0900

    update scene

--- playground/control/control_richtextlabel_auto_resize.gd
+++ playground/control/control_richtextlabel_auto_resize.gd
@@ -19,6 +19,9 @@ func _ready():
 	pam.add_action( "Change Text 1", Key.KEY_1, func(): change_text( "1234567890" ) )
 	pam.add_action( "Change Text 2", Key.KEY_2, func(): change_text( "a" ) )
 	pam.add_action( "Change Text 3", Key.KEY_3, func(): change_text( "a\n1234567890\nabcdefghijklmnopqrstuvwxyz\npoi098" ) )
+	pam.add_action( "Change Text 4", Key.KEY_4, func(): change_text( "BBCode [color=green]test[/color]\n[b]{text}[/b]\n[i]{text}[/i]\n[u]{text}[/u]" ) )
+	pam.add_lf()
+	pam.add_action( "BB : Toggle", Key.KEY_Q, func(): $RichTextLabel.bbcode_enabled = !$RichTextLabel.bbcode_enabled )
 	build_summary( eSceneType.TEST )
 	
 	#
@@ -32,5 +35,7 @@ func change_text( str ):
 	$Label.text = "minimum_size : " + str( $RichTextLabel.get_minimum_size() )
 	$Label.text += "\n"
 	$Label.text += "size : " + str( $RichTextLabel.size )
+	$Label.text += "\n"
+	$Label.text += "custom minimum size : " + str( $RichTextLabel.custom_minimum_size )
 	
 	$RichTextLabel/helper_move2center.do.call_deferred()
