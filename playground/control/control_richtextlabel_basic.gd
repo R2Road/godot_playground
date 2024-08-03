b8b6d3f1022a07956e9eed25c7474f71e781aaa1
R2Road <studior2road@gmail.com>
2024-08-03 21:13:17 +0900

    add scene : control_richtextlabel_basic

---
+++ playground/control/control_richtextlabel_basic.gd
@@ -0,0 +1,22 @@
+# REF : https://docs.godotengine.org/en/stable/tutorials/ui/bbcode_in_richtextlabel.html
+
+class_name control_richtextlabel_basic extends GDPTScene
+
+
+
+######################### GDPT Override ##########################
+static func scene_name()->String:
+	return "RichTextLabel"
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( control_richtextlabel_basic.scene_name() )
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_lf()
+	pam.add_action( "BB : Toggle", Key.KEY_1, func():
+		$RichTextLabel1.bbcode_enabled = !$RichTextLabel1.bbcode_enabled
+		$RichTextLabel2.bbcode_enabled = !$RichTextLabel2.bbcode_enabled
+	)
+	build_summary( eSceneType.TEST )
--- playground/control/control_root.gd
+++ playground/control/control_root.gd
@@ -12,4 +12,5 @@ func _ready():
 	pam.add_mover( control_label_align.scene_name(), 			Key.KEY_2, 	"res://control/control_label_align.tscn" )
 	pam.add_mover( control_nine_patch_rect_basic.scene_name(),	Key.KEY_3, 		"res://control/control_nine_patch_rect_basic.tscn" )
 	pam.add_mover( control_progress_bar_basic.scene_name(),		Key.KEY_4, 		"res://control/control_progress_bar_basic.tscn" )
+	pam.add_mover( control_richtextlabel_basic.scene_name(),	Key.KEY_5, 		"res://control/control_richtextlabel_basic.tscn" )
 	build_summary( eSceneType.ROOT )
