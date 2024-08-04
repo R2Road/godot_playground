4cff367fb68740e4d8f8b2a2fb8cc9674876d84b
R2Road <studior2road@gmail.com>
2024-08-05 01:54:46 +0900

    update scene : script_path

--- playground/script/script_path.gd
+++ playground/script/script_path.gd
@@ -7,11 +7,24 @@ static func scene_name()->String:
 	return "Script Path"
 
 
+static func scene_path()->String:
+	var s = get_stack()[0]["source"]
+	return str( s )
+
+
 
 ############################ Override ############################
 func _ready():
 	pam.set_name( script_path.scene_name() )
-	pam.add_split()
+	pam.add_lf()
 	pam.add_back( Key.KEY_ESCAPE )
-	pam.add_split()
+	pam.add_lf()
 	build_summary( eSceneType.TEST )
+	
+	#
+	#
+	#
+	$Label.text = "get path in static func : " + script_path.scene_path()
+	$Label.text += "\n\n"
+	$Label.text += "get path in script obj : " + get_script().get_path()
+	$Label/helper_move2center.do()
