dd1af2f468a3c52b612e10fb5dfa0e0b136dad25
R2Road <studior2road@gmail.com>
2024-08-06 03:52:26 +0900

    update scene : rotation_tracking

--- playground/various/rotation_tracking.gd
+++ playground/various/rotation_tracking.gd
@@ -12,6 +12,12 @@ static func scene_path()->String:
 
 
 
+############################ Variable ############################
+var target_direction : Vector2
+var on_tracking = false
+
+
+
 ############################ Override ############################
 func _ready():
 	pam.set_name( rotation_tracking.scene_name() )
@@ -28,11 +34,34 @@ func _ready():
 	update_pivot( $Target.position )
 
 
+func _process( delta ):
+	if $Pivot.rotation != $Tracker.rotation:
+		var s = $Pivot.rotation - $Tracker.rotation
+		if 0 < s:
+			$Tracker.rotation += 1 * delta
+			if 0 > $Pivot.rotation - $Tracker.rotation:
+				$Tracker.rotation = target_direction.angle()
+		elif 0 > s:
+			$Tracker.rotation -= 1 * delta
+			if 0 < $Pivot.rotation - $Tracker.rotation:
+				$Tracker.rotation = target_direction.angle()
+		
+		$Target/T.text = str( $Tracker.rotation ).substr( 0, 5 )
+		$Target/S.text = str( s ).substr( 0, 5 )
+
+
 ############################   User   ############################
 func update_pivot( target_position ):
-	var direction = ( target_position - $Center.position )
+	target_direction = ( target_position - $Center.position )
 	
-	$Pivot.rotation = direction.angle()
-	$Pivot.point_end.x = direction.length()
+	$Pivot.rotation = target_direction.angle()
+	$Pivot.point_end.x = target_direction.length()
+	$Tracker.point_end.x = target_direction.length()
 	
-	$Target/Label.text = str( direction.angle() ).substr( 0, 5 )
+	$Target/P.text = str( target_direction.angle() ).substr( 0, 5 )
+	
+	var d1 = $Pivot.rotation - $Tracker.rotation
+	print( "d1 : " + str( d1 ) )
+	
+	#
+	on_tracking = $Pivot.rotation != $Tracker.rotation
