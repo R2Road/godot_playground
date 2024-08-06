54acea2c2bd5109f23a5e5326b13aa36cc344490
R2Road <studior2road@gmail.com>
2024-08-07 01:05:37 +0900

    add scene : control_slider_h_basic

---
+++ playground/control/control_slider_h_basic.gd
@@ -0,0 +1,29 @@
+class_name control_slider_h_basic extends GDPTScene
+
+
+
+######################### GDPT Override ##########################
+static func scene_name()->String:
+	return "H Slider : Basic"
+
+
+static func scene_path()->String:
+	return super.scene_path()
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( control_slider_h_basic.scene_name() )
+	pam.add_split()
+	pam.add_back( Key.KEY_ESCAPE )
+	build_summary( eSceneType.TEST )
+	
+	#
+	# Start Practice
+	#
+	_on_h_slider_value_changed( $HSlider.value )
+
+
+func _on_h_slider_value_changed( _value ):
+	$HSlider/Label.text = str( _value )
