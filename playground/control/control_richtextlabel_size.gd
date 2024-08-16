b493dee38c8a604130ab41b06515335b35929184
R2Road <studior2road@gmail.com>
2024-08-17 07:53:56 +0900

    update scene

--- playground/control/control_richtextlabel_size.gd
+++ playground/control/control_richtextlabel_size.gd
@@ -22,6 +22,9 @@ func _ready():
 	pam.add_action( "Change Text 4", Key.KEY_4, func(): change_text( "BBCode [color=green]test[/color]\n[b]{text}[/b]\n[i]{text}[/i]\n[u]{text}[/u]" ) )
 	pam.add_lf()
 	pam.add_action( "BB : Toggle", Key.KEY_Q, func(): $RichTextLabel.bbcode_enabled = !$RichTextLabel.bbcode_enabled )
+	pam.add_lf()
+	pam.add_message( "Node의 Text를 교체한다고 size 값이 바로 갱신되는 것이 아니다." )
+	pam.add_message( "크기 변경을 확실히 감지해서 반응하려면 \"minimum_size_changed\" signal을 이용해야만 한다." )
 	build_summary( eSceneType.TEST )
 	
 	#
@@ -31,6 +34,8 @@ func _ready():
 
 
 func change_text( str ):
+	print( "change_text : Start" )
+	
 	$RichTextLabel.text = str
 	$Label.text = "minimum_size : " + str( $RichTextLabel.get_minimum_size() )
 	$Label.text += "\n"
@@ -39,3 +44,11 @@ func change_text( str ):
 	$Label.text += "custom minimum size : " + str( $RichTextLabel.custom_minimum_size )
 	
 	$RichTextLabel/helper_move2center.do.call_deferred()
+	
+	print( "change_text : End" )
+
+
+
+############################  Signal  ############################
+func _on_rich_text_label_minimum_size_changed() -> void:
+	print( "_on_rich_text_label_minimum_size_changed : Called"  )
