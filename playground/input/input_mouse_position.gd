3cf3ab77dd6099c16fa088803829a6f86ac90619
R2Road <studior2road@gmail.com>
2024-08-19 18:16:52 +0900

    update scene : input_mouse_position

--- playground/input/input_mouse_position.gd
+++ playground/input/input_mouse_position.gd
@@ -17,12 +17,10 @@ func _ready():
 	pam.add_message( "> get_viewport().get_mouse_position() : Viewport 위의 좌표는 Camera 이동에 영향 받지 않는다." )
 	pam.add_message( "> CanvasItem.get_global_mouse_position() : CanvasItem이 제공하는 mouse의 월드 좌표." )
 	pam.add_lf()
-	pam.add_action( "Camera U", Key.KEY_W, func(): $helper_reticle.position.y -= 10 )
-	pam.add_action( "Camera D", Key.KEY_S, func(): $helper_reticle.position.y += 10 )
-	pam.add_action( "Camera L", Key.KEY_A, func(): $helper_reticle.position.x -= 10 )
-	pam.add_action( "Camera R", Key.KEY_D, func(): $helper_reticle.position.x += 10 )
-	pam.add_lf()
-	pam.add_message( "> 초록점이 0, 0" )
+	pam.add_action( "Camera U", Key.KEY_W, func(): $helper_0_0.position.y -= 10 )
+	pam.add_action( "Camera D", Key.KEY_S, func(): $helper_0_0.position.y += 10 )
+	pam.add_action( "Camera L", Key.KEY_A, func(): $helper_0_0.position.x -= 10 )
+	pam.add_action( "Camera R", Key.KEY_D, func(): $helper_0_0.position.x += 10 )
 	build_summary( eSceneType.TEST )
 
 
