f208aff6577e3ef278cbb282b01b79e3535f2ac4
R2Road <studior2road@gmail.com>
2024-09-18 06:32:54 +0900

    ...

--- playground/resolution/resolution___change_window_size.gd
+++ playground/resolution/resolution___change_window_size.gd
@@ -14,7 +14,7 @@ static func scene_path()->String:
 
 ############################ OnReady #############################
 @onready var resolution_item_list : ItemList = $CanvasLayer/ResolutionItemList
-@onready var stretch_mode_item_list : ItemList = $CanvasLayer/StretchModeItemList
+@onready var content_scale_mode_item_list : ItemList = $CanvasLayer/ContentScaleModeItemList
 
 
 
@@ -26,7 +26,7 @@ var resolution_list : Dictionary = {
 	, "1280 x 720" : Vector2i( 1280, 720 )
 	, "1920 x 1080" : Vector2i( 1920, 1080 )
 }
-var stretch_mode_list : Dictionary = {
+var content_scale_mode_list : Dictionary = {
 	  "Disabled" : Window.ContentScaleMode.CONTENT_SCALE_MODE_DISABLED
 	, "Canvas Items" : Window.ContentScaleMode.CONTENT_SCALE_MODE_CANVAS_ITEMS
 	, "Viewport" : Window.ContentScaleMode.CONTENT_SCALE_MODE_VIEWPORT
@@ -63,11 +63,11 @@ func _ready()->void:
 		current_item_index += 1
 	
 	current_item_index = 0
-	for i in stretch_mode_list:
-		stretch_mode_item_list.add_item( i )
+	for i in content_scale_mode_list:
+		content_scale_mode_item_list.add_item( i )
 		
-		if default_content_scale_mode == stretch_mode_list[i]:
-			stretch_mode_item_list.select( current_item_index )
+		if default_content_scale_mode == content_scale_mode_list[i]:
+			content_scale_mode_item_list.select( current_item_index )
 		
 		current_item_index += 1
 
@@ -84,6 +84,6 @@ func _on_resolution_item_list_item_selected( index: int )->void:
 	get_window().size = new_resolution
 
 
-func _on_stretch_mode_item_list_item_selected(index: int) -> void:
-	var new_stretch_mode = stretch_mode_list[stretch_mode_item_list.get_item_text( index )]
-	get_tree().root.content_scale_mode = new_stretch_mode
+func _on_content_scale_mode_item_list_item_selected(index: int) -> void:
+	var new_content_scale_mode = content_scale_mode_list[content_scale_mode_item_list.get_item_text( index )]
+	get_tree().root.content_scale_mode = new_content_scale_mode
