6519b0acd3490ef3e358fe9474fce5fc17220596
R2Road <studior2road@gmail.com>
2024-09-21 04:25:06 +0900

    update scene

--- playground/file/config/config___section.gd
+++ playground/file/config/config___section.gd
@@ -2,12 +2,6 @@ class_name config___section extends GDPTScene
 
 
 
-############################ Variable ############################
-var path = "res://file/config/config___section_helper.cfg"
-var config = ConfigFile.new()
-
-
-
 ######################### GDPT Override ##########################
 static func scene_name()->String:
 	return "Config : Section"
@@ -28,24 +22,41 @@ func _ready():
 	#
 	# Start Practice
 	#
-	config.load( path )
 	
-	$Label.text = ( "[get_sections()]" + "\n" )
-	$Label.text += "\n"
-	$Label.text += "\n"
+	var source_string : String = "
+		[section_1]
+		name = \"a b c d e f g\"
+		age = 10
+		position = Vector2( 10, 222 )
+
+		[section_2]
+		name = \"1 2 3 4 5\"
+		age = 20
+		position = Vector2( 222, 10 )
+
+		[section_3]
+	"
 	
+	# Ready
+	var config = ConfigFile.new()
+	
+	# Parse
+	config.parse( source_string )
+	
+	$Label4Sections.text = "+ get_sections()"
+	$Label4Sections.text += "\n"
+	$Label4Sections.text += "\n"
+	
+	# Show Sections
 	for s in config.get_sections():
-		$Label.text += s
-		$Label.text += "\n"
+		$Label4Sections.text += s
+		$Label4Sections.text += "\n"
 	
-	#
-	#
-	#
-	var file = FileAccess.open( path, FileAccess.READ )
-	if file:
-		$Label4Source.text = ( "[" + path + "]" + "\n" )
-		$Label4Source.text += "\n"
-		$Label4Source.text += "\n"
-		$Label4Source.text += file.get_as_text()
+	# Source String
+	$Label4Source.text = ( "+ Source String" + "\n" )
+	$Label4Source.text += "\n"
+	$Label4Source.text += source_string
 	
+	# Code Edit
+	$helper_code.show_code( scene_path().replace( ".tscn", ".gd" ), 40, 53 )
 	
