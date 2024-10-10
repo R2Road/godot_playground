e6762db977d118cd6f224bfa8e84a624078bca47
R2Road <studior2road@gmail.com>
2024-10-10 23:04:48 +0900

    update scene

--- playground/script/script___runtime_parse_and_run.gd
+++ playground/script/script___runtime_parse_and_run.gd
@@ -2,6 +2,11 @@ class_name script___runtime_parse_and_run extends GDPTScene
 
 
 
+### OnReady ######################################################
+@onready var code_edit = $CodeEdit
+
+
+
 ### Variable #####################################################
 
 
@@ -22,19 +27,13 @@ func _ready():
 	pam.add_lf()
 	pam.add_back( Key.KEY_ESCAPE )
 	pam.add_lf()
+	pam.add_action( "Run Code", Key.KEY_SPACE, do )
 	build_summary( eSceneType.TEST )
 	
 	#
 	#
 	#
-	do()
-
-
-
-### Interface ####################################################
-func do():
-	var script = GDScript.new()
-	script.source_code = \
+	code_edit.text = \
 "class_name AA extends RefCounted
 
 var label : Label
@@ -46,13 +45,23 @@ func do()->void:
 	label.text += str( self.get_script().get_script_method_list() )
 	label.get_node_or_null( \"helper_move2center\" ).do()
 "
-	script.reload()
+
+
+
+### Interface ####################################################
+func do():
+	var script = GDScript.new()
+	script.source_code = code_edit.text
 	
-	var r = Resource.new()
-	r.set_script( script )
-	
-	self.get_script()
-	
-	
-	r.label = $Label
-	r.do()
+	var parse_result = script.reload()
+	if Error.OK == parse_result:
+		var r = Resource.new()
+		r.set_script( script )	
+		
+		r.label = $Label
+		r.do()
+	else:
+		$Label.text = \
+		  "[Failed] : Error Code - " \
+		+ str( parse_result )
+		$Label/helper_move2center.do()
