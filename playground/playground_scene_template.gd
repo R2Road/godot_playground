85f74c088e42f16fc7268016a058d110be9aa1bd
R2Road <studior2road@gmail.com>
2024-11-13 06:06:26 +0900

    ...

--- playground/playground_scene_template.gd
+++ playground/playground_scene_template.gd
@@ -1,30 +1,26 @@
 extends GDPTScene
 
 
-
-############################  Export  ############################
-
-
-
-############################ ReadOnly ############################
-
-
-
-############################ Variable ############################
+### Summary ######################################################
+### Const ########################################################
+### Export #######################################################
+### ReadOnly #####################################################
+### Signal #######################################################
+### Variable #####################################################
 
 
 
-############################ Override ############################
+### Override #####################################################
 func _ready():
 	pam.set_name( "PlayGround" )
 	pam.add_split()
 	pam.add_exit()
 	pam.add_lf()
-	pam.add_root( basic_root.scene_name(),		Key.KEY_1, 		basic_root.scene_path() )
+	#pam.add_root( basic_root.scene_name(),		Key.KEY_1, 		basic_root.scene_path() )
 	pam.add_lf()
-	pam.add_root( audio_root.scene_name(),		Key.KEY_Q, 		audio_root.scene_path() )
+	#pam.add_root( audio_root.scene_name(),		Key.KEY_Q, 		audio_root.scene_path() )
 	pam.add_lf()
-	pam.add_root( "ETC",		Key.KEY_Z, 		"res://2d/etc/etc_root.tscn" )
+	#pam.add_root( "ETC",		Key.KEY_Z, 		"res://2d/etc/etc_root.tscn" )
 	pam.add_split()
 	build_summary( eSceneType.ROOT )
 	
@@ -36,8 +32,8 @@ func _ready():
 
 
 
-############################   User   ############################
+### Setup : Interface ############################################
+### Static : Interface ###########################################
+### Interface ####################################################
 
-
-
-############################  Signal  ############################
+### Signal : Receiver ############################################
