bb2064dca3fb89ff224575a4b4d4a2c3288f9991
R2Road <studior2road@gmail.com>
2025-01-17 14:21:45 +0900

    update scene : input_keyboard_in_input

--- playground/input/input_keyboard_in_input.gd
+++ playground/input/input_keyboard_in_input.gd
@@ -14,26 +14,42 @@ func _ready():
 	#
 	# Start Practice
 	#
-	UpdateMessage( "Press Keyboard" )
+	UpdatePressdMessage( "Press Keyboard" )
+	UpdateReleasedMessage( "Press Keyboard" )
 
 
 func _input(event):
-	if !(event is InputEventKey ):
+	if !( event is InputEventKey ):
 		return
 	
-	if !event.is_pressed():
+	if event.is_pressed():
+		ProcessKeyPressed( event )
 		return
-		
-	match event.keycode:
-		_:
-			UpdateMessage( "key pressed : " + event.as_text() )
+	
+	if event.is_released():
+		ProcessKeyReleased( event )
+		return
 
 
 ############################   User   ############################
-func UpdateMessage( arg ):
-	var message_node = get_node( "Message" )
-	message_node.text = str( arg )
+func ProcessKeyPressed( _key_event : InputEventKey ):
+	UpdatePressdMessage( "key pressed : " + _key_event.as_text() )
+
+func UpdatePressdMessage( _message : String ):
+	var message_node = get_node( "PressedMessage" )
+	message_node.text = _message
 	message_node.set_position(
-			( get_viewport().size * 0.5 )
+			( get_viewport().size * 0.3 )
+			- ( message_node.get_minimum_size() * message_node.get_scale() * 0.5 )
+	)
+
+func ProcessKeyReleased( _key_event : InputEventKey ):
+	UpdateReleasedMessage( "key released : " + _key_event.as_text() )
+
+func UpdateReleasedMessage( _message : String ):
+	var message_node = get_node( "ReleasedMessage" )
+	message_node.text = _message
+	message_node.set_position(
+			( get_viewport().size * 0.6 )
 			- ( message_node.get_minimum_size() * message_node.get_scale() * 0.5 )
 	)
