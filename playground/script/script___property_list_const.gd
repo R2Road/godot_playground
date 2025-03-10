1791a5fc43d8dab1f21b36788a7fcbbb3eeba66b
R2Road <studior2road@gmail.com>
2025-03-10 16:31:28 +0900

    add scene : script___property_list_const

---
+++ playground/script/script___property_list_const.gd
@@ -0,0 +1,40 @@
+class_name script___property_list_const extends GDPTScene
+
+
+
+######################### GDPT Override ##########################
+static func scene_name()->String:
+	return "Script : Property List : Const"
+
+
+static func scene_path()->String:
+	return super.scene_path()
+
+
+
+############################ Override ############################
+func _ready():
+	pam.set_name( scene_name() )
+	pam.add_lf()
+	pam.add_back( Key.KEY_ESCAPE )
+	pam.add_lf()
+	pam.add_action( "do", Key.KEY_1, do )
+	pam.add_lf()
+	pam.add_message( "Const 멤버는 Script 객체의 get_script_constant_map() 으로 접근한다." )
+	pam.add_lf()
+	build_summary( eSceneType.TEST )
+	
+	# Code Edit
+	$CanvasLayer/helper_code.show_code( scene_path().replace( ".tscn", "___helper.gd" ), 1, 10 )
+	$CanvasLayer/helper_code2.show_code( scene_path().replace( ".tscn", ".gd" ), 38, 40 )
+
+
+
+############################   User   ############################
+func do()->void:
+	#
+	# Setup 2 Resource Class
+	#
+	var helper_script : Script = script___property_list_const___helper.new().get_script()
+	for c in helper_script.get_script_constant_map():
+		$CanvasLayer/helper_output.print( c )
---
+++ playground/script/script___property_list_const___helper.gd
@@ -0,0 +1,9 @@
+class_name script___property_list_const___helper extends Resource
+
+
+
+### Variable #####################################################
+const b : bool = true
+const i : int = 0
+const f : float = 0.0
+const s : String = "str"
