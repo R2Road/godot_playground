ab2ac0a8f6cae4d378b917bb3efb8cc4688fbd12
R2Road <studior2road@gmail.com>
2025-03-10 18:05:45 +0900

    update scene : gdscript___thread___basic

--- playground/gdscript/gdscript___thread___basic.gd
+++ playground/gdscript/gdscript___thread___basic.gd
@@ -30,6 +30,7 @@ class ThreadHelper extends RefCounted:
 		limit_time = _limit_sec * 1000
 		
 		last_time = Time.get_ticks_msec()
+		elapsed_time = 0
 	
 	func update()->bool:
 		elapsed_time += ( Time.get_ticks_msec() - last_time )
@@ -52,13 +53,16 @@ func _ready():
 	pam.add_back( Key.KEY_ESCAPE )
 	pam.add_lf()
 	pam.add_action( "Do", Key.KEY_1, do )
+	pam.add_lf2()
+	pam.add_message( "한 번 처리 종료된 thread는 start() 함수를 불러도 할당한 함수에 진입을 안한다." )
+	pam.add_message( "재사용이 안되는 것 같다." )
 	pam.add_split()
 	build_summary( eSceneType.TEST )
 	
 	#
 	# Code Edit
 	#
-	helper_code.show_code( scene_path().replace( ".tscn", ".gd" ), 1, 10 )
+	helper_code.show_code( scene_path().replace( ".tscn", ".gd" ), 68, 92 )
 
 
 func _process(delta: float) -> void:
@@ -71,6 +75,12 @@ func _process(delta: float) -> void:
 func do()->void:
 	if thread.is_alive():
 		return
+	
+	#
+	# 한 번 처리 종료된 thread는 start() 함수를 불러도 할당한 함수에 진입을 안한다.
+	# 재사용이 안되는 것 같다.
+	#
+	thread = Thread.new()
 	
 	thread_helper.start( 3 )
 	thread.start( thread_function.bind( thread_helper ) )
